a35c01a9be0fa138d5c3b01003be04fd4d748fa4
KoD1407 <denkovtun2008007@gmail.com>
2022-12-04 00:54:09 +0200

    First attempt

---
+++ Denys Kovtun/Lesson 2/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+byte x1 = 2;
+
+short x2 = 3;
+
+int x3 = 4;
+
+long x4 = 5;
+
+bool x5 = true;
+
+char x6 = "$";
+
+float x7 = 8;
+
+double x8 = 9;
+
+decimal x9 = 10;
+
+string x10 = "qwerty";
+
+
+Math.Abs(x2);
+
+Console.WriteLine("123");
+Console.ReadLine();
