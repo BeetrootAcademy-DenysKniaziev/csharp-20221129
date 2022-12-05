ec61bd6f2a9ec2015587c221275347ccc6579d83
AlexMelnykov <alex.niten@gmail.com>
2022-12-05 22:44:07 +0200

    OleksiiMelnykov.Lesson2

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,8 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Denys!");
+﻿/* Lesson 2
+ Homework */
 
 byte b1 = 20;
 byte b2 = 173;
 //Console.WriteLine(b1+b2);
 
+short s1 = -17000;
+short s2 = 3800;
+//Console.WriteLine(s1-s2);
+
+int i1 = 154000000;
+int i2 = 56;
+//Console.WriteLine(i1/i2);
+
+long l1 = -158456;
+long l2 = 72;
+//Console.WriteLine(l1*l2);
+
+bool bl1 = true;
+bool bl2 = true;
+bool bl3 = false;
+bool bl4 = false;
+
+//Console.WriteLine(bl1&&bl2);
+//Console.WriteLine(bl1 && bl3);
+//Console.WriteLine(bl4 || bl3);
+//Console.WriteLine(!bl1);
+
+
+char ch = '#';
+//Console.WriteLine(ch);
+
+float f1 = 18.9f;
+double d1 = 27.3;
+decimal dc = 5.3m;
+
+//Console.WriteLine(f1+d1);
+//Console.WriteLine(f1/d1);
+
+string str1 = "Beetroot";
+//Console.WriteLine(str1);
 
