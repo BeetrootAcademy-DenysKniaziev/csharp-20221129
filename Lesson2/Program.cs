3abee22cb4aee765cb03be80ad01384f91dcab57
AlexMelnykov <alex.niten@gmail.com>
2022-12-05 21:37:25 +0200

    OleksiiMelnykov.Lesson2

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Denys!");
+
+byte b1 = 20;
+byte b2 = 173;
+//Console.WriteLine(b1+b2);
+
+
