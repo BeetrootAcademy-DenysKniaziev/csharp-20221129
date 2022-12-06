c85152d4ca6dd2ec6969a104ea5240574cb97f59
Artemsushko <114439320+Artemsushko@users.noreply.github.com>
2022-12-06 23:15:08 +0100

    Artem Sushko. Homework and kvestGame

---
+++ Artem Sushko/Lesson3/Lesson3 Game/Program.cs
@@ -0,0 +1,98 @@
+﻿Console.Write("Enter your name: ");
+string name = Console.ReadLine();
+bool finish = false;
+
+
+while (!finish)
+{
+    Console.Clear();
+    Console.Write($"{name}, do you want to travel?(CHOOSE: Y(Yes) or N(No): ");
+    string ans = Console.ReadLine();
+    if (ans == "Y")
+    {
+        while (true)
+        {
+
+            Console.Clear();
+            Console.Write($"{name}, where do you want to go? (F(Forest) or M(Mountains)): ");
+            string choice = Console.ReadLine();
+            if (choice == "F")
+            {
+                while (true)
+                {
+
+                    Console.Clear();
+                    Console.Write($"{name} select the way! (CHOOSE: L(Left) or R(Right)): ");
+                    string choice2 = Console.ReadLine();
+                    if (choice2 == "L")
+                    {
+                        Console.WriteLine($"{name}, you have found a bandit camp! Unlucky. GAME IS OVER!!!");
+                        finish = true;
+                        break;
+                    }
+                    else if (choice2 == "R")
+                    {
+                        Console.WriteLine($"{name}, you have found a beautiful place! Enjoy and have a rest:)");
+                        finish = true;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong Input");
+                    }
+                    
+                    Console.ReadLine();
+                }
+                break;
+            }
+            else if (choice == "M")
+            {
+                while (true)
+                {
+
+                    Console.Clear();
+                    Console.Write($"{name} select the way! (CHOOSE: L(Left) or R(Right)): ");
+                    string choice2 = Console.ReadLine();
+                    if (choice2 == "L")
+                    {
+                        Console.WriteLine($"{name}, you have found a beautiful mountain river! Enjoy and have a rest:)");
+                        finish = true;
+                        break;
+                    }
+                    else if (choice2 == "R")
+                    {
+                        Console.WriteLine($"{name}, you have met a mountain Lion! Unlucky. GAME IS OVER!!!");
+                        finish = true;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong Input");
+                    }
+                    Console.ReadLine();
+                }
+                break;
+
+            }
+            else
+            {
+                Console.WriteLine("Wrong Input");
+            }
+            Console.ReadLine();
+            
+        }
+    }
+    else if (ans == "N")
+    {
+        Console.WriteLine($"OK, {name}, stay home. BYE!");
+        break;
+    }
+    else 
+    { 
+        Console.WriteLine("Wrong Input");
+    }
+    Console.ReadLine();
+    
+}
+Console.WriteLine("Come again!");
+
---
+++ Artem Sushko/Lesson3/Lesson3 Hometask/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Enter first value");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second value");
+int y = int.Parse(Console.ReadLine());
+
+int min, max;
+int sum = 0;
+
+if (x!=y)
+{
+    if (x > y)
+    {
+        min = y;
+        max = x;
+    }
+    else
+    {
+        min = x;
+        max = y;
+    }
+    for(int i = min; i <= max; i++)
+    {
+        sum += i;
+    }
+    Console.WriteLine("Sum of numbers: " + sum);
+}
+else
+{
+    Console.WriteLine("Sum: " + x);
+}
