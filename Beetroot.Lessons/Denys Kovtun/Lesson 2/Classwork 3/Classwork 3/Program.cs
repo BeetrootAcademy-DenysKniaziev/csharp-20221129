7624a415827cd8a880652f0af989d8199dc3e5a2
KoD1407 <denkovtun2008007@gmail.com>
2022-12-06 21:20:25 +0200

    Classwork

---
+++ Beetroot.Lessons/Denys Kovtun/Lesson 2/Classwork 3/Classwork 3/Program.cs
@@ -0,0 +1,113 @@
+﻿#region If Else
+/*using System.Linq.Expressions;
+
+int a = 7;
+
+if (a == 5)
+{
+    Console.WriteLine("a = 5");
+}
+else if(a == 7)
+{
+    Console.WriteLine("a = 7");
+
+}
+else
+{
+    Console.WriteLine("a = else");
+
+}
+
+
+switch (a)
+{
+    case 5:
+        Console.WriteLine("a = 5");
+        break;
+
+    case 7:
+        Console.WriteLine("a = 7");
+        break;
+
+
+    default:
+        Console.WriteLine("a = else");
+        break;
+}
+
+int b = 0;
+
+if (a == 5)
+{
+    b = a * 2;
+}
+else if (a == 7)
+{
+    b = a * 3;
+
+}
+else
+{
+    b = 1;
+}
+
+switch (a)
+{
+    case 5:
+        b = a * 2;
+        break;
+
+        case 7:
+
+        b = a * 3;
+        break;
+
+    default:
+        b = 1;
+        break;
+}
+
+b = 0;
+
+b = a switch
+{
+    5 => a * 2,
+    7 => a * 3,
+    _ => 1
+
+};
+
+if (a == 5)
+{
+    b = a * 2;
+
+}
+else
+{
+    b = 1;
+}
+//  /|\ = \|/
+b = a == 5 ? a * 2 : 1;
+
+Console.WriteLine($"B = {b}");
+
+int c = 1;
+
+int d = 2;
+
+int e = 3;
+
+if (c == 1) Console.WriteLine("c = 1");
+
+if (c == 1 && d == 2 || e == 3)
+{
+    Console.WriteLine("c == 1 && d = 2 || e == 3");
+}
+#endregion
+*/
+#endregion
+
+Console.WriteLine("What is your name???");
+string input = Console.ReadLine();
+Console.WriteLine($"Hello {input}");
+
