1c5d037ed37f5b7c0880492d001d8c55d4db717c
SteranOo <denisknyazev96@gmail.com>
2022-12-08 19:09:01 +0100

    Denys Kniaziev. Lesson 4. Classwork

---
+++ Denys Kniaziev/Lesson4/Lesson4.Classwork/Program.cs
@@ -0,0 +1,51 @@
+﻿class Program
+{
+    static void PrintHello()
+    {
+        Console.WriteLine("Hello!");
+    }
+
+    static void PrintHello(string name)
+    {
+        Console.WriteLine($"Hello, {name}!");
+    }
+
+    static int Square(int x) => x * x;
+
+    static void DoubleNumber(ref int number)
+    {
+        number *= 2;
+    }
+
+    static bool ParseInt(string str, out int value) => int.TryParse(str, out value);
+
+    static bool ParseIntOrDefault(string str, out int value, int defaultValue)
+    {
+        if (int.TryParse(str, out value))
+            return true;
+
+        value = defaultValue;
+        return false;
+    }
+
+    static void Main()
+    {
+        PrintHello();
+        PrintHello("Denys");
+
+        var x = 5;
+        var y = Square(x);
+
+        Console.WriteLine(y);
+        Console.WriteLine(Square(5));
+
+        var number = 100;
+        DoubleNumber(ref number);
+        Console.WriteLine(number);
+
+        var input = Console.ReadLine();
+
+        var isParsed = ParseIntOrDefault(input, out var intValue, -1);
+        Console.WriteLine($"Parsed: {isParsed}, Value: {intValue}");
+    }
+}
