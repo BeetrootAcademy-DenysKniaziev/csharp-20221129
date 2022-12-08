cc7a2e7b2112f8a51f4b2c9f6b16df7768912395
Sedjex <kathshem@gmail.com>
2022-12-08 19:08:46 +0200

    Homework 3

---
+++ root/L-3 Homework/L-3 Homework/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+int d1;
+int d2;
+int sum = 0;
+
+Console.WriteLine("Hi, bro!");
+Console.WriteLine("You need to choose 2 positive integers, and I will calculate for you the sum of these numbers and all the numbers between them.If they are equal then sum should be one of them ");
+Console.WriteLine("\nSo, what is your first number? ");
+
+
+try
+{
+d1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nCool! What is your second number? ");
+d2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (d1 == d2)
+{
+    sum = d1;
+}
+else
+{
+    for (int i = Math.Min(d1, d2); i <= Math.Max(d1, d2); i++)
+    {
+        sum += i;
+    }
+}
+
+    Console.WriteLine("\nSum is: "+ sum);
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine("\nIt's not a number, bro!");
+}
