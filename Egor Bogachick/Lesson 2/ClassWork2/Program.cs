e4259d1462fdfe03cea9b622e950338d45b5c7a1
Egor <egorbf2003@gmail.com>
2022-12-08 03:40:57 +0200

    Egor Bogachik. Class work and home work

---
+++ Egor Bogachick/Lesson 2/ClassWork2/Program.cs
@@ -0,0 +1,42 @@
+﻿byte b = 1;
+short s = 34;
+int i = 9;
+long l = 678;
+
+sbyte sb = -5;
+ushort us = 5;
+uint ui = 5;
+ulong ul = 6;
+
+float f = 1.1f;
+double d = 2.3;
+decimal m = 2.4m;
+
+bool b1 = false;
+bool b2 = true;
+
+int x1 = -b;
+
+b++;
+++b;
+
+Console.WriteLine(b);
+
+int x2 = 5;
+int x3 = 10;
+Console.WriteLine(x2 * x3);
+Console.WriteLine(x2 / x3);
+Console.WriteLine(x2 % x3);
+
+Console.WriteLine(x2 << 2);
+Console.WriteLine(x2 >> 2);
+
+Console.WriteLine(x2 > x3);
+Console.WriteLine(x2 < x3); 
+Console.WriteLine(x2 == x3);
+Console.WriteLine(x2 != x3);
+
+Console.WriteLine(x2 & x3);
+Console.WriteLine(x2 ^ x3);
+
+Console.WriteLine(b1 && b2);
---
+++ Egor Bogachick/Lesson 2/HomeWork2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.OutputEncoding = System.Text.Encoding.Unicode; // для розкадки
+
+Console.Write("Ведіть x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ведіть y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"\n Перший приклад -6*x^3+5*x^2-10*x+15: {Math.Round(-6 * Math.Pow(x, 3) + 5 * Math.Pow(x, 2) - 10 * x + 15, 2)}");
+Console.WriteLine($" Другий приклад abs(x)*sin(x): {Math.Round(Math.Abs(x) * Math.Sin(x), 2)}");
+Console.WriteLine($" Третій приклад 2*pi*x: {Math.Round(2 * Math.PI * x, 2)}");
+Console.WriteLine($" Четвертий приклад max(x, y): {Math.Max(x, y)}");
