74d31908bcc1335cab4dda01cd7de0ebf130cded
SkyLineR1001 <119454635+SkyLineR1001@users.noreply.github.com>
2022-12-08 23:04:08 +0200

    Lesson4.CW

--- Serhii Rubayko/Lesson 4/Lesson4.Classwork/Program.cs
+++ Serhii Rubayko/Lesson 4/Lesson4.Classwork/Program.cs
@@ -1,8 +1,65 @@
-﻿void PrintHello()
+﻿class Program
 {
-    Console.WriteLine("Hello!");
- }
+    static void PrintHello()
+    {
+        Console.WriteLine("Hello!");
+    }
 
-PrintHello();
-PrintHello();
-PrintHello();
+    static void PrintHello(string name)
+    {
+        Console.WriteLine($"Hello, {name}!");
+    }
+
+    static int Square(int x) => x * x;
+
+    static void DoubleNumber(ref int number)
+
+    {
+        number *= 2;
+    }
+
+    static bool ParseInt(string str, out int value) => int.TryParse(str, out value);
+   
+    static bool ParseIntOrDefault(string str, out int value, int defaultValue)
+
+    {
+        if (int.TryParse(str, out value))
+            return true;
+
+        value = defaultValue;
+        return false;
+    }
+
+
+
+    static void Main()
+    {
+        PrintHello();
+        PrintHello("Denys");
+
+        var x = 5;
+        var y= Square (x);
+
+        Console.WriteLine(y);
+        Console.WriteLine(Square(5));
+
+        var number = 100;
+        DoubleNumber(ref number);
+        Console.WriteLine(number);
+
+        var input = Console.ReadLine();
+
+        //int intValue;
+       var isParsed = int.TryParse(input, out var intValue);
+
+        Console.WriteLine($"Parsed: {isParsed}, Value: {intValue}");
+
+
+
+        
+        Console.ReadKey();
+    }
+}
+
+
+//Console.ReadKey();
