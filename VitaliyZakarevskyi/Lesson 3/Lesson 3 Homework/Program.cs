ead6c6f90def3e56a59f26989457ad7f52ebe25d
vitovskyii <zakarevskiy@gmail.com>
2022-12-09 14:23:41 +0200

    Lesson 3 Homework

---
+++ VitaliyZakarevskyi/Lesson 3/Lesson 3 Homework/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics.SymbolStore;
+using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+Console.WriteLine("Hello, Denys!");
+
+
+int sum = 0;
+
+
+Console.WriteLine("Insert X value");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Insert Y value");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x < y)
+{
+    for (int i = x; i <= y; i++)
+    {
+        sum += i;
+        Console.WriteLine(sum);
+    }
+}
+if (x > y)
+{
+    for (int i = y; i <= x; i++)
+    {
+        sum += i;
+        Console.WriteLine(sum);
+    }
+}
+
+
+
+
+
+
