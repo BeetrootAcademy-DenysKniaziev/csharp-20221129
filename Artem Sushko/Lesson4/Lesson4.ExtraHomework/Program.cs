26dd32afe7ffcf2cdb933f3b1c6817cbe620fd22
Artemsushko <114439320+Artemsushko@users.noreply.github.com>
2022-12-09 21:36:57 +0100

    Artem Sushko.ExtraHomework4

---
+++ Artem Sushko/Lesson4/Lesson4.ExtraHomework/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+
+    static string Repeat(string X, int N)
+    {
+        if (N < 1) return " ";
+
+        return X+Repeat(X, N-1);
+    }
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.Write("Enter your text: ");
+            string X = Console.ReadLine();
+            Console.Write("\nEnter amount of Repeats: ");
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine(Repeat(X, N));
+            Console.ReadLine();
+        }
+    }
+}
