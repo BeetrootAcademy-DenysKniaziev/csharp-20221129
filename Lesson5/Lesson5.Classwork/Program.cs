42c9d16d16888600ce604926693f95900fea89af
Recrut79 <adv3079@gmail.com>
2022-12-10 12:49:11 +0200

    CreateNewProject

---
+++ Lesson5/Lesson5.Classwork/Program.cs
@@ -0,0 +1,20 @@
+﻿
+class Program
+{
+
+    static void PrintHello()
+    {
+        Console.WriteLine("Hello");
+    }
+
+    static void PrintHello(string name)
+    {
+        Console.WriteLine($"Heloo, {name}");
+    }
+
+    static void Main()
+    {
+        PrintHello();
+        PrintHello("Piter");
+    }
+}
