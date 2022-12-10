1a4ec0a96119ec9642a4fdb9b7da6ae42a213deb
SkyLineR1001 <119454635+SkyLineR1001@users.noreply.github.com>
2022-12-10 15:06:37 +0200

    RubaykoSerhiiLesson2Homework

--- Serhii Rubayko/Lesson 2/Lesson 2 - Homework/Program.cs
+++ Serhii Rubayko/Lesson 2/Lesson 2 - Homework/Program.cs
@@ -1,21 +1,106 @@
-﻿
-
-//Double D = 12.286;
-//Double Da = Math.Abs(D);
+﻿#region integer numbers
 
 byte b1 = 30;
 byte b2 = 4;
 
-//double d = (double)b1 / (double)b2;
+Console.WriteLine(b1 / b2);
+
+short s1 = -300;
+short s2 = 105;
+
+short s3 = short.MaxValue;
+short s4 = short.MinValue;
+
+double Result = s4 / s3;
+
+Console.WriteLine("{0}/{1}={2}", short.MinValue, short.MaxValue, Result);
+
+Console.WriteLine(s1 * s2);
+Console.WriteLine(s1 % s2);
+
+int i1 = 20000000;
+int i2 = -3200000;
+
+Console.WriteLine(i1 + (i2-s2));
+
+long l1=i1*i2;
+long L = long.MaxValue;
+
+Console.WriteLine(l1^b2);
+Console.WriteLine(L);
+
+#endregion
+
+#region Floating-point
+float f1 = 1.5e4f;
+
+bool bo1 = f1 == b1;
+
+Console.WriteLine(bo1);
+
+double d = (double)b1 / (double)b2;
 
 Console.WriteLine((double)b1 / (double)b2);
 
 
+decimal D = decimal.MaxValue;
+
+Console.WriteLine("{0},{1},{2}", D, decimal.MinValue, 
+    (double)decimal.MaxValue/double.MinValue);
+
+#endregion
+
+#region Char String
+string a = "hello";
+string b = "h";
+// Append to contents of 'b'
+b += "ello";
+Console.WriteLine(a == b);
+Console.WriteLine(object.ReferenceEquals(a, b));
+
+string str = "bullsHit";
+
+char ch = str[5];
+
+Console.WriteLine(ch);
+
+for (int i=0;i<str.Length; i++)
+{
+    Console.Write(str[i] + " ");
+}
+
+Console.WriteLine();
+
+Console.WriteLine(Math.Abs(-4.7));
+
+int x = 5;
+int y = 10;
+Console.WriteLine($"Max of betwin {x} and {y} is {Math.Max(x, y)}," +
+    $" Min of betwin {x} and {y} is {Math.Min(x, y)}");
+#endregion
+
+
+double X = -2.71828;
+
+double Y = -6 * Math.Pow(X, 3) + 5 * Math.Pow(X, 2) - 10 * X + 15;
+
+Console.WriteLine(Y);
+
+Console.WriteLine(Math.Abs(X)*Math.Sin(X));
+
+Console.WriteLine(2*Math.PI *X);
+
+Console.WriteLine($"Max of betwin {X} and {Y} is {Math.Max(X, Y)}");
+
+////
 
 
 
-//double x = 2.5;
+////Double D = 12.286;
+////Double Da = Math.Abs(D);
 
-//Console.WriteLine(D);
-//Console.WriteLine(Da);
+////double x = 2.5;
+
+////Console.WriteLine(D);
+////Console.WriteLine(Da);
 Console.ReadKey();
