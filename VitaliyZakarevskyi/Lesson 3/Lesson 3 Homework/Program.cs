4f90cd74d00cd6dcb8052c968cce08bd5f0ab7b2
vitovskyii <zakarevskiy@gmail.com>
2022-12-11 16:36:16 +0200

    Lesson 4 Homework

--- VitaliyZakarevskyi/Lesson 3/Lesson 3 Homework/Program.cs
+++ VitaliyZakarevskyi/Lesson 3/Lesson 3 Homework/Program.cs
@@ -11,9 +11,9 @@ int sum = 0;
 
 
 Console.WriteLine("Insert X value");
-int x = Convert.ToInt32(Console.ReadLine());
+    int x = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Insert Y value");
-int y = Convert.ToInt32(Console.ReadLine());
+    int y = Convert.ToInt32(Console.ReadLine());
 
 if (x < y)
 {
