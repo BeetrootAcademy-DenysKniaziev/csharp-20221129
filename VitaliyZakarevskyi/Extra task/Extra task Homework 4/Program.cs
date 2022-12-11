3494ca46427236d3864a38c179eea28a7f1b8b09
vitovskyii <zakarevskiy@gmail.com>
2022-12-11 22:02:49 +0200

    Extra task

---
+++ VitaliyZakarevskyi/Extra task/Extra task Homework 4/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Repeat("str", 3);
+    }
+    static void Repeat(string input, int numberOfTimes)
+    {
+        for(int i = 0; i < numberOfTimes; i++)
+        {
+            Console.Write(input);
+        }
+        
+    }
+}
