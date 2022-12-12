fba3477905254e61767a85829364a3f56ff06b97
kovasnik <egorbf2003@gmail.com>
2022-12-12 05:03:42 +0200

    Bogachik Egor. HomeWork3

---
+++ Egor Bogachick/Lesson3/Lesson3.HomeWork/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+Console.Write("Введіть значення х:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введіть значення y:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int sum = Math.Min(x, y);
+int i = Math.Min(x, y);
+
+while (i != Math.Max(x, y))
+{
+    i++;
+    sum += i;
+} ;
+
+Console.Write($"Сумма= {sum}");
