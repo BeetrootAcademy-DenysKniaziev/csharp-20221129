a2f3f0f83aa29c394f2dd3c9b71ec85b3d37423f
Artemsushko <114439320+Artemsushko@users.noreply.github.com>
2022-12-12 14:28:00 +0100

    Artemsushko.Classwork

---
+++ Artem Sushko/Lesson3/Lesson3 Classwork/Program.cs
@@ -0,0 +1,105 @@
+﻿#region Math dt
+
+//int x = -5;
+//int a = Math.Abs(x);
+//double b = Math.Sqrt(3);
+////Console.WriteLine(b);
+
+//DateTime dt = new DateTime(1900, 5, 10, 17, 55, 14);
+//dt = DateTime.Now;
+//dt = dt.AddDays(-1);
+//TimeSpan ts = new TimeSpan(5, 4, 0, 0);
+//dt = dt.Add(ts);
+
+//Console.WriteLine(dt);
+
+#endregion
+
+#region if else switch
+//int a = 7;
+
+//if (a==5)
+//{
+//    Console.WriteLine("a=5");
+//}
+//else if (a==7)
+//    Console.WriteLine("a=7");
+//else
+//{
+//    Console.WriteLine("a=else");
+//}
+
+
+//switch (a)
+//{
+//    case 5:
+//        Console.WriteLine("a=5");
+//        break;
+
+//    case 7:
+//        Console.WriteLine("a=7");
+//        break; 
+
+//    default:
+//        Console.WriteLine("a=default");
+//        break;
+//}
+
+//int b = 0;
+
+//if (a == 5)
+//{
+//    b = a * 2;
+//}
+//else if (a == 7)
+//{
+//    b = a * 3;
+//}
+//else
+//{
+//    b = 1;
+//}
+
+//b = 0;
+
+//switch (a)
+//{
+
+//    case 5:
+//        b = a * 2;
+//        break;
+//    default:
+//        b = 1;
+//        break;
+//}
+
+//b = 0;
+//b = a switch
+//{
+//    5 => a * 2,
+//    7 => a * 3,
+//    _ => 1
+//} ;
+//Console.WriteLine($"B = {b}");
+
+//b = 0;
+// b= a==5 ? a*2 : 1;
+
+//Console.WriteLine($"B = {b}");
+
+
+//int c = 1;
+//int d = 2;
+//int e = 3;
+
+//if (c==1)
+//{
+//    Console.WriteLine("c==1");
+//}
+//if (d==2)
+//    Console.WriteLine("d==2");
+#endregion
+Console.Write("Your name is: ");
+string input = Console.ReadLine();
+Console.WriteLine($"Hello {input}");
+
