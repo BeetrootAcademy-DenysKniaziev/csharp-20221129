f09abe557cbf47b64a703b30685ad8e9335a8354
Egor Bogachik <79566284+kovasnik@users.noreply.github.com>
2022-12-15 15:59:06 +0200

    Egor Bogachik. Homework 4 (#55)

---
+++ Egor Bogachick/Lesson4/Lesson4.ClassWork/Program.cs
@@ -0,0 +1,119 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void PrintHello()
+    {
+        Console.WriteLine("Hello!");
+    }
+
+    static void PrintHello(string name)
+    {
+        Console.WriteLine($"Hello, {name}!");
+    }
+
+    static int Square(int x) => x * x;
+
+    static void DoubleNumber(ref int number)
+    {
+        number *= 2;
+    }
+
+    static bool ParseInt(string str, out int value) => int.TryParse(str, out value);
+
+    static bool ParseIntOrDefault(string str, out int value, int defaultValue)
+    {
+        if (int.TryParse(str, out value))
+            return true;
+
+        value = defaultValue;
+        return false;
+    }
+
+    //n! = 1 * 2 * … * n
+    static int Factorial(int n)
+    {
+        var result = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+
+    //n! = 1 * 2 * … * n
+    //fact(5) = 120 = fact(4) * 5 = 24 * 5
+    static int FactorialRec(int n)
+    {
+        if (n <= 1)
+            return 1;
+
+        return n * FactorialRec(n - 1);
+    }
+    //0, 1, 2, 3, 4, 5, 6
+    //0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+    static int Fibonachi(int n)
+    {
+        if (n == 0 || n == 1) return n;
+        return Fibonachi(n - 1) + Fibonachi(n - 2);
+    }
+
+    static void Main()
+    {
+        //printhello();
+        //printhello("denys");
+
+        //var x = 5;
+        //var y = square(x);
+
+        //console.writeline(y);
+        //console.writeline(square(5));
+
+        //var number = 100;
+        //doublenumber(ref number);
+        //console.writeline(number);
+
+        //var input = console.readline();
+
+        //var isparsed = parseintordefault(input, out var intvalue, -1);
+        //console.writeline($"parsed: {isparsed}, value: {intvalue}");
+
+        var timer = new Stopwatch();
+        timer.Start();
+        int tmp;
+
+        for (int k = 0; k < 100000; k++)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                //Console.WriteLine($"Fact({i}) = {Factorial(i)}");
+                tmp = Factorial(i);
+            }
+        }
+
+        timer.Stop();
+
+        Console.WriteLine($"Time: {timer.Elapsed}");
+
+        timer.Reset();
+        timer.Start();
+
+        for (int k = 0; k < 100000; k++)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                //Console.WriteLine($"Fact({i}) = {FactorialRec(i)}");
+                tmp = FactorialRec(i);
+            }
+        }
+
+        timer.Stop();
+
+        Console.WriteLine($"Time: {timer.Elapsed}");
+
+        for (int i = 0; i < 20; i++)
+        {
+            Console.WriteLine($"Fibonachi({i}) = {Fibonachi(i)}");
+        }
+    }
+}
