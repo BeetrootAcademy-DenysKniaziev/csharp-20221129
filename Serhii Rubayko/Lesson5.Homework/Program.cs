38e86ec00133b4607723bb30acbfb84b2e5bb4ed
SkyLineR1001 <119454635+SkyLineR1001@users.noreply.github.com>
2022-12-15 23:39:13 +0200

    2

--- Serhii Rubayko/Lesson5.Homework/Program.cs
+++ Serhii Rubayko/Lesson5.Homework/Program.cs
@@ -4,16 +4,13 @@
     
     static int GreatestCommonDivisor(int x, int y)
     {
-      
-        if (x % 2 == 0 && y % 2 == 0)
-        {
-           
 
-            return 2;
-
-
-        }
-        else return 1;
+        if (x == 1 || y == 1) return 1;
+        if (x%2==0&&y%2==0)
+           return 2*GreatestCommonDivisor(x/2,y/2); 
+            
+        //    return 1;
+        //else return 2;
 
     }
 
@@ -21,9 +18,13 @@
 
 
     static void Main()
-    { 
-    
-    
+    {
+
+        var x = 4;
+        var y = 2;
+
+        Console.WriteLine($"Greater common divisor {x} & {y} is {GreatestCommonDivisor(x, y)}");
+
     
     
     
