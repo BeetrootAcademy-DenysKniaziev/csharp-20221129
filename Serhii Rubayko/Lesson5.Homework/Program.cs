82c394b844f848741ac19a94119802d2786d23d0
SkyLineR1001 <119454635+SkyLineR1001@users.noreply.github.com>
2022-12-16 07:02:12 +0200

    2

--- Serhii Rubayko/Lesson5.Homework/Program.cs
+++ Serhii Rubayko/Lesson5.Homework/Program.cs
@@ -1,13 +1,40 @@
 ﻿class Program
 {
-   
-    
+
+    static int Factorial(int n)
+    {
+        var result = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+
+    static int FactorialRec(int n)
+    {
+        if (n <= 1)
+            return 1;
+
+        return n * FactorialRec(n - 1);
+    }
+
+    static int Fibonachi(int n)
+    {
+        if (n == 0 || n == 1) return n;
+
+        return Fibonachi(n - 1) + Fibonachi(n - 2);
+    }
+
     static int GreatestCommonDivisor(int x, int y)
     {
 
-        if (x == 1 || y == 1) return 1;
-        if (x%2==0&&y%2==0)
-           return 2*GreatestCommonDivisor(x/2,y/2); 
+        for (int i=0; )
+        
+        
+        //if (x == 1 || y == 1) return 1;
+        //else if (x%2==0&&y%2==0)
+        //   return 2; 
             
         //    return 1;
         //else return 2;
