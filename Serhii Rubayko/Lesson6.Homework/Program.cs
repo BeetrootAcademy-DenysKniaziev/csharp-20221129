90c8503e0e2eca56b144c215af4389de1d5b3534
SkyLineR1001 <119454635+SkyLineR1001@users.noreply.github.com>
2022-12-18 22:32:30 +0200

    class

---
+++ Serhii Rubayko/Lesson6.Homework/Program.cs
@@ -0,0 +1,57 @@
+﻿
+int[] FirstArr= new int[7] {7, 34, 76, 11, 23, 4, 17};
+
+Console.WriteLine();
+Console.Write("My 1st array members: \t");
+foreach (int i in FirstArr)
+{
+       Console.Write("\t{0}", i);
+}
+
+//var arr = FirstArr[2];
+
+Index begin= new Index(2);
+Index end= new Index(2,true);
+
+var third = FirstArr[begin];
+
+var beforeLast = FirstArr[end];
+
+Console.WriteLine();
+Console.WriteLine($"Length - \"{FirstArr.Length}\", i{begin} - {third}, i{end} - {beforeLast}");
+
+
+var threeElements = FirstArr[0..3];
+
+var threeLast = FirstArr[^3..^0];
+
+foreach (int i in FirstArr[0..3])
+{
+    Console.Write("\t{0}", i);
+}
+
+Console.Write("\t\t");
+
+
+foreach (int i in FirstArr[^3..^0])
+{
+    Console.Write("\t{0}", i);
+}
+
+Range rng=new Range(begin, end);
+
+Console.WriteLine();
+
+foreach (int i in FirstArr[rng])
+{
+    Console.Write("\t{0}", i);
+}
+
+Console.WriteLine(); 
+
+Console.WriteLine($"Length - {FirstArr[rng].Length}");
+
+
+Console.WriteLine();
+
+
