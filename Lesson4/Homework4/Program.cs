4a0ed12a5528fba6fd36135373daa910eb9c047a
DmytroBonislavskyi <119468981+IMBomb@users.noreply.github.com>
2022-12-19 23:39:05 +0100

    Dmytro Bonislavskyi add Homework4 to Lesson4 (#52)
    * Lesson2 Homework
    
    * Request for X add
    
    * Homework for lesson3 added
    
    * Changed dirs for previus homeworks and add beta of RPG
    
    * Homework 5 added
    
    * Homework 5 added
    
    * Homework4
    
    * Homewor4 to Lesson4
    
    * Homework6 add
    
    * Homework 7 add

---
+++ Lesson4/Homework4/Program.cs
@@ -0,0 +1,75 @@
+﻿// See https://aka.ms/new-console-template for more information
+//using System.Threading;
+
+//Define and call with different parameters next methods:
+
+//Method that will return max value among all the parameters
+//… min value …
+//Method TrySumIfOdd that accepts 2 parameters and returns true if sum of numbers between inputs is odd, otherwise false, sum return as out parameter
+//Overload first two methods with 3 and 4 parameters
+//Extra:
+
+//Define and call with different parameters next methods:
+
+//Method Repeat that will accept string X and number N and return X repeated N times (e.g. Repeat(‘str’, 3) returns ‘strstrstr’ = ‘str’ three times)
+
+using System;
+//using static Data_Structures_Console.AbstractAndInterface;
+//using static Data_Structures_Console.AbstractAndInterface;
+
+namespace functions
+{
+
+    class Program
+    {
+        public static class OneGoodClass
+        {
+
+            public static decimal Pmax(decimal[] numb)
+            { return numb.Max(); }
+
+            public static decimal Pmin(decimal[] numb)
+            { return numb.Min(); }
+
+            public static decimal Pmax(decimal a, decimal b, decimal c)
+            { return Math.Max(Math.Max(a, b), c); }
+
+            public static decimal Pmax(decimal a, decimal b, decimal c, decimal d)
+            { return Math.Max(Math.Max(Math.Max(a, b), c),d); }
+
+            public static decimal Pmin(decimal a, decimal b, decimal c)
+            { return Math.Min(Math.Min(a, b), c); }
+
+            public static decimal Pmin(decimal a, decimal b, decimal c, decimal d)
+            { return Math.Min(Math.Min(Math.Min(a, b), c), d); }
+
+            public static bool TrySumIfOdd(out int aa,out int bb) {
+                aa = 3; bb = 2;
+                if ((aa + bb )% 2 != 0) return true;
+                else return false;
+            }
+
+            public static string Repeat(string text, int reps)  {
+                string newtext ="";
+                for(int i = 1; i <= reps; i++)
+                { newtext += text; }
+                return newtext;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            decimal[] numb = { 4, 7, 10 };
+            //decimal Pmax = takeMax(decimal a, decimal b) => Math.Max(a,b)
+            //.Aggregate(1, (int interim, int next) => interim * next);
+
+            Console.WriteLine(OneGoodClass.Pmax(numb));
+            Console.WriteLine(OneGoodClass.Pmax(2, 3, 5));
+            Console.WriteLine(OneGoodClass.Pmax(2, 7, 5, 3));
+            Console.WriteLine(OneGoodClass.TrySumIfOdd(out int a, out int b));
+            Console.WriteLine(OneGoodClass.Repeat("_BeetRoot",3));
+
+        }
+    }
+}
+
---
+++ Lesson7/Homework7/Program.cs
@@ -0,0 +1,81 @@
+﻿//create next methods:
+//Compare that will return true if 2 strings are equal, otherwise false, but do not use build-in method
+//Analyze that will return number of alphabetic chars in string, digits and another special characters
+//Sort that will return string that contains all characters from input string sorted in alphabetical order (e.g. 'Hello' -> 'ehllo')
+//Duplicate that will return array of characters that are duplicated in input string (e.g. 'Hello and hi' -> ['h', 'l'])
+
+using System;
+using System.Text;
+
+namespace Homework
+{
+    public class Homework7
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Statment that string one is equal to string two is " + Compare("a", "aa"));
+            Console.WriteLine();
+            string text = " Krokodyl 144 .~~";
+            var analyzeRes = Analyze(text);
+            Console.WriteLine("Letters " + (analyzeRes).Item1 + "\nDigits " + (analyzeRes).Item2 + "\nOther " + (analyzeRes).Item3);
+            Console.WriteLine("Sorted output: " + Sort("frtAb!!cc__b azy3xwvu  улш коацй tsrqpon74587:'mlkjihgfedcba")); //zyxwvutsrqponmlkjihgfedcba
+            Console.WriteLine(Duplicate("frtAb!!cc__b azy3xwvu  улш коацй tsrqpon74587:'mlkjihgfedcba"));
+        }
+
+        //Method Compare
+        static bool Compare(string a, string b) {
+            if (a.Length == b.Length) {
+                for (int i = 0; i < a.Length; i++) {
+                    if (a[i] != b[i]) return false;
+                }
+            }
+            else return false;
+            return true;
+        }
+
+        //Method Analyze
+        static (int, int, int) Analyze(string a) {
+            int nC = 0, nD = 0;
+            foreach (char c in a) {
+                if (char.IsLetter(c)) nC++;
+                if (char.IsDigit(c)) nD++;
+            }
+            return (nC, nD, a.Length - nC - nD);
+        }
+
+        //Method Sort
+        static string Sort(string a) {
+            string cleanA = "";
+            foreach (var cA in a) if (char.IsLetter(cA)) cleanA += cA;
+            var b = (cleanA.ToLower()).ToCharArray();
+            string ext = "";
+            foreach (char letter in sorting(b)) ext += letter;
+            return ext;
+        }
+        //Function sorting for Sort method task
+        static char[] sorting(char[] a) {
+            var b = a;
+            for (int i = 0; i < b.Length - 1; i++) {
+                for (int j = 0; j < b.Length - 1 - i; j++) {
+                    if (b[j] > b[j + 1]) (b[j], b[j + 1]) = (b[j + 1], b[j]);
+                }
+            }
+            return b;
+        }
+        //Method Duplicate
+        static char [] Duplicate(string a)
+        {
+            int bLanght = a.Length;
+            string temp;
+            string duplicates = "";
+
+            for (; a.Length > 0;)  {
+                temp = a[0].ToString();
+                a = a.Replace(a[0].ToString(), "");
+                if ((bLanght - a.Length) > 1) duplicates += temp;
+                bLanght= a.Length;  
+            }
+            return duplicates.ToCharArray();
+        }
+    }
+}
