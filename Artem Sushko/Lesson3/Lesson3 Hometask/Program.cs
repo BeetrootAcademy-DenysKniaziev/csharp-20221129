d2584ede7faa6767084ea3d0222feac3423f88ea
Artemsushko <114439320+Artemsushko@users.noreply.github.com>
2022-12-20 00:25:29 +0100

    Artem Sushko. Homework and kvestGame (#17)
    * Artem Sushko. Homework and kvestGame
    
    * Artemsushko.ExtraHomework3
    
    * Artemsushko.Classwork

---
+++ Artem Sushko/Lesson3/Lesson3 Hometask/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Enter values");
+
+if (!int.TryParse(Console.ReadLine(), out var x) || !int.TryParse(Console.ReadLine(), out var y))
+{
+    Console.WriteLine("Invalid input");
+}
+else
+{
+
+    int min, max;
+    int sum = 0;
+
+    if (x != y)
+    {
+        if (x > y)
+        {
+            min = y;
+            max = x;
+        }
+        else
+        {
+            min = x;
+            max = y;
+        }
+        for (int i = min; i <= max; i++)
+        {
+            sum += i;
+        }
+        Console.WriteLine("Sum of numbers: " + sum);
+    }
+    else
+    {
+        Console.WriteLine("Sum: " + x);
+    }
+}
