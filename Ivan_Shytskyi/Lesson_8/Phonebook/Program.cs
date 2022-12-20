bedce321434e31f11aabf1d5126043435500891e
shytskyi <111076900+shytskyi@users.noreply.github.com>
2022-12-20 10:46:22 +0100

    1

--- Ivan_Shytskyi/Lesson_8/Phonebook/Program.cs
+++ Ivan_Shytskyi/Lesson_8/Phonebook/Program.cs
@@ -1,6 +1,9 @@
-﻿const string FileName = "Phonebook.txt";
+﻿using System.Collections.Generic;
 
-Console.WriteLine("Phone book:");
+const string FileName = "Phonebook.txt";
+
+Console.WriteLine("Phone book Application");
+Console.WriteLine("select actions:");
 Console.WriteLine("1 - Show phone book");
 Console.WriteLine("2 - Add record");
 Console.WriteLine("3 - UpdateRecord");
@@ -16,7 +19,12 @@ while (true)
     switch (actions.Key)
     {
         case ConsoleKey.D1:
+            Console.WriteLine("Phone book:");
             ShowPhonebook(records);
+            break;
+        case ConsoleKey.D0:
+            Console.WriteLine("Exit");
+            Environment.Exit(0);
             break;
         default:
             Console.WriteLine("Incorect input!");
@@ -32,9 +40,19 @@ void ShowPhonebook((string FirstName, string LastName, string number)[] records)
     foreach (var record in records)
         Console.WriteLine($"First name:{record.FirstName}, Last name: {record.LastName}, number: {record.number}");
 }
-void AddRecord()
+(string FirstName, string LastName, string number)[] AddRecord((string FirstName, string LastName, string number)[] records)
 {
-
+    var newData = new (string FirstName, string LastName, string number)[records.Length+1];
+    string FirstName = "";
+    string LastName = "";
+    string number = "";
+    for (int i = 0; i < 3; i++)
+    {
+        Console.WriteLine($"Enter First name, Last name, number");
+        newData[i] = $"{FirstName=Console.ReadLine()}|{LastName=Console.ReadLine()}|{number=Console.ReadLine()}";
+    }
+    var records = SaveToFile(newData);
+    return records;
 }
 void UpdateRecord()
 {
