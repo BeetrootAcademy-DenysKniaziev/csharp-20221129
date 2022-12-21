d9601287ca4ede4d0c93ce4b991b8de31583196f
Recrut79 <adv3079@gmail.com>
2022-12-21 12:54:06 +0200

    DiachenkoAnatolii.implemetedExtratask

--- HomeWork6/DiachenkoAnatolii-HomeWork6/Helper.cs
+++ HomeWork6/DiachenkoAnatolii-HomeWork6/Helper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,15 +22,59 @@ namespace DiachenkoAnatolii_HomeWork6
 
         public static void RunSorting(this int[] myArray)
         {
+            //Console.WriteLine($"Original array is:");
+            //PrintSorting(myArray);
+
+            //Console.WriteLine($"Selection array is:");
+            //PrintSorting(SelectionAlgorithm(myArray));
+
+            //myArray = myArray.InputData();
+
+            //Console.WriteLine($"Original array is:");
+            //PrintSorting(myArray);
+
+            //Console.WriteLine($"Bubble array is:");
+            //PrintSorting(BubbleAlgorithm(myArray));
+
+            //myArray = myArray.InputData();
+
+            //Console.WriteLine($"Original array is:");
+            //PrintSorting(myArray);
+
+            //Console.WriteLine($"Insertion array is:");
+            //PrintSorting(InsertionAlgorithm(myArray));
+
+            myArray = myArray.InputData();
             Console.WriteLine($"Original array is:");
             PrintSorting(myArray);
-            Console.WriteLine($"Selection array is:");
-            PrintSorting(SelectionAlgorithm(myArray));
-            Console.WriteLine($"Bubble array is:");
-            PrintSorting(BubbleAlgorithm(myArray));
-            Console.WriteLine($"Insertion array is:");
-            PrintSorting(InsertionAlgorithm(myArray));
 
+            SortAlgorithmType algorithmType = ChoiceAlgorithm($"Please choice algorithm of sorting: 1.{nameof(SortAlgorithmType.SelectionAlgorithm)}, 2.{nameof(SortAlgorithmType.BubbleAlgorithm)}, 3.{nameof(SortAlgorithmType.InsertionAlgorithm)}");
+
+            OrderBy orderBy = ChoiceOrderType("Enter type if sort: 1.Asc, 2.Desc");
+
+            PrintSorting(myArray.Sort(algorithmType, orderBy));
+        }
+
+        public static SortAlgorithmType ChoiceAlgorithm(string message)
+        {
+            SortAlgorithmType index;
+            do
+            {
+                Console.WriteLine(message);
+
+            } while (!Enum.TryParse(Console.ReadLine(), out index));
+            return index;
+        }
+
+        public static OrderBy ChoiceOrderType(string message)
+        {
+            OrderBy index;
+            do
+            {
+                Console.WriteLine(message);
+
+            } while (!Enum.TryParse(Console.ReadLine(), out index));
+            return index;
         }
 
         public static void PrintSorting(this int[] myArray)
@@ -43,7 +88,7 @@ namespace DiachenkoAnatolii_HomeWork6
 
         public static int[] SelectionAlgorithm(this int[] myArray)
         {
-            for (int i = 0; i < myArray.Length; i++)
+            for (int i = 0; i < myArray.Length -1; i++)
             {
 
                 for (int j = i + 1; j < myArray.Length; j++)
@@ -59,14 +104,17 @@ namespace DiachenkoAnatolii_HomeWork6
 
         public static int[] BubbleAlgorithm(this int[] myArray)
         {
-            for (int i = 0; i < myArray.Length -1; i++)
+            for (int i = 0; i < myArray.Length; i++)
             {
-                    if (myArray[i] > myArray[i + 1])
+                for (int j = 0; j < myArray.Length - i - 1; j++)
+                {
+                    if (myArray[j] > myArray[j + 1])
                     {
-                    myArray.Swap(i, i + 1);
-                }
-            }            
-           return myArray;
+                        myArray.Swap(j, j + 1);
+                    }
+                }                
+            }
+            return myArray;
         }
 
         public static int[] InsertionAlgorithm(this int[] myArray)
@@ -88,6 +136,41 @@ namespace DiachenkoAnatolii_HomeWork6
             var temp = myArray[index1];
             myArray[index1] = myArray[index2];
             myArray[index2] = temp;           
-        }     
+        }
+        
+        public enum SortAlgorithmType
+        {
+            SelectionAlgorithm = 1,
+            BubbleAlgorithm = 2,
+            InsertionAlgorithm = 3,
+        }
+
+        public enum OrderBy
+        {
+            Asc = 1,
+            Desc = 2
+        }
+
+        public static int[] Sort(this int[] myArray, SortAlgorithmType sortType, OrderBy orderBy)
+        {
+            switch(sortType)
+            {
+                case SortAlgorithmType.SelectionAlgorithm:
+                    myArray = myArray.SelectionAlgorithm();
+                    break;
+                case SortAlgorithmType.BubbleAlgorithm:
+                    myArray = myArray.BubbleAlgorithm();
+                    break;
+                case SortAlgorithmType.InsertionAlgorithm:
+                    myArray = myArray.InsertionAlgorithm();
+                    break;
+            }
+
+            if (orderBy == OrderBy.Desc)
+            {
+                 return myArray.Reverse().ToArray();
+            }
+            return myArray;
+        }           
     }
 }
