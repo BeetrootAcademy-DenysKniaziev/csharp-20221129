3e4615154f09c171ea277ac880ddc8be3a7955b9
shytskyi <111076900+shytskyi@users.noreply.github.com>
2022-12-22 20:47:43 +0100

    1

--- Ivan_Shytskyi/Lesson_10/Lesson_10.Classwork/Program.cs
+++ Ivan_Shytskyi/Lesson_10/Lesson_10.Classwork/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Person
+{
+    private string name;
+
+    public string Name 
+    { 
+        get { return $"Mr.{name}"; }
+        set { name = value; } 
+    }
+
+    public string LastName { get; set; }
+
+    public string FullName { get { return Name + " " + LastName; } }
+
+    public void SayHello()
+    {
+        Console.WriteLine("Hello");
+    }
+    public void Say(string text)
+    {
+        Console.WriteLine(text);
+    }
+}
+
+class Program
+{
+    static void Main ()
+    {
+        Console.WriteLine("Lesson 10");
+
+        var p1 = new Person();
+        p1.Name = "Ivan";
+        p1.LastName = "Shytskyi";
+        Console.WriteLine(p1.Name);
+        Console.WriteLine(p1.LastName);
+        Console.WriteLine(p1.FullName);
+        p1.SayHello();
+        p1.Say("Hi!!");
+        var p2  = new Person();
+        {
+            
+        }
+    }
+}
