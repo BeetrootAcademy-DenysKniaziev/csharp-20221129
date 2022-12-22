9b69077337070bea9be0bfd26d205b731f8e0829
KoD1407 <119446178+KoD1407@users.noreply.github.com>
2022-12-22 21:31:33 +0200

    First attempt (#6)
    * First attempt
    
    * Homework 2
    
    * Homework 2
    
    * Homework2
    
    * Homework2
    
    * Homework 2
    
    * Homework 2
    
    * fix gitignore
    
    * folder move
    
    * delete files on remote
    
    * fix gitignore
    
    * readme fix
    
    Co-authored-by: SteranOo <denisknyazev96@gmail.com>

---
+++ Denys Kovtun/Lesson 2/Homework 2/Homework 2/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+byte x1 = 2;
+
+short x2 = 3;
+
+int x3 = 4;
+
+long x4 = 54356L;
+
+bool x5 = true;
+
+char x6 = '$';
+
+float x7 = 8F;
+
+double x8 = 9.45D;
+
+decimal x9 = 10;
+
+string x10 = "qwerty";
+*/
+
+
+
+
+
+//homework//
+double x = 5;
+
+double y = 7;
+
+
+double firstTaskValue = (-6 * Math.Pow(x, 3) + 5 * Math.Pow(x, 2) - 10 * x + 15);
+Console.WriteLine("If x = 5; -6*x^3+5*x^2-10*x+15 = " + firstTaskValue);
+
+
+double secondTaskValue = Math.Abs(x) * Math.Sin(x);
+Console.WriteLine("If x = 5; abs(x)*sin(x) = " + secondTaskValue);
+
+
+double thirdTaskValue = 2 * Math.PI * x;
+Console.WriteLine("If x = 5; 2*pi*x = " + thirdTaskValue);
+
+double fourthTaskValue = Math.Max(x, y);
+Console.WriteLine("If x = 5 and y = 7; max(x, y) = " + fourthTaskValue);
