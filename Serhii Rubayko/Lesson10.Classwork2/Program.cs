f03386dbab93071bf11639c7c3f5c1113b1fcb12
SkyLineR1001 <119454635+SkyLineR1001@users.noreply.github.com>
2022-12-22 22:00:19 +0200

    Classwork

---
+++ Serhii Rubayko/Lesson10.Classwork2/Program.cs
@@ -0,0 +1,53 @@
+﻿class Location
+{
+    public double X { get; set;}
+
+    public double Y { get; set;}    
+
+
+}
+
+
+
+
+
+
+class Galaxy
+{
+    public string Name { get; set; }
+
+    public Location Location { get; set; }
+
+    public void Collapse()
+    {
+        Console.WriteLine($"Galaxy{Name} has been collapsed");
+    }
+}
+
+
+
+
+class Planet
+{
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public double Temperature { get; set; }
+
+    public double Gravity { get; set; }
+
+    public int LocalYear { get; set; }
+
+    public Galaxy Galaxy { get; set; }
+
+    public Planet ParentPlanet { get; set; }
+
+    public void PassOneYear ()
+    {
+        LocalYear++;
+
+    }
+}
+
+
