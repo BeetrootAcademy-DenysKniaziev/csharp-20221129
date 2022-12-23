06ec2354a156a560893896a87f41ca6098f5214f
Roman Bychkov <roman.bychkov.1921@gmail.com>
2022-12-23 03:23:43 +0200

    Added clases

---
+++ Roman Bychkov/Lesson10/Lesson10.HomeWork/Program.cs
@@ -0,0 +1,48 @@
+﻿class Program
+{
+    static void Main()
+    {
+
+    }
+}
+public class PublishingHouse
+{
+    public string Name { get; set; }
+    public int YearOfFoundation { get; set; }
+    public Country Country { get; set; }
+
+}
+public class Country
+{
+    public string Name { get; set; }
+
+    public double Square { get; set; }
+
+    public string Language { get; set; };
+}
+
+public class Genre
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
+public class Book
+{
+    public string Name { get; set; }
+    public int CountOfPages { get; set; }
+    public Author Author { get; set; }
+    public PublishingHouse PubHouse { get; set; }
+}
+public class Author
+{
+    public string Name { get; set; }
+    public string LastName { get; set; }
+
+    public DateTime DateOfBorn { get; set; } = DateTime.Now;
+
+    public int Age
+    {
+        get => DateTime.Now.Year - DateOfBorn.Year;
+    }
+    public Country CountryOfBorn { get; set; }
+}
