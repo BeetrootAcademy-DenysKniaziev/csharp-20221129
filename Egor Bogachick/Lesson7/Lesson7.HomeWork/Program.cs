cd40101e99692250ca36f9948ca1fa77e2b78b74
kovasnik <egorbf2003@gmail.com>
2022-12-23 03:58:56 +0200

    Egor Bogachik. Homework7

--- Egor Bogachick/Lesson7/Lesson7.HomeWork/Program.cs
+++ Egor Bogachick/Lesson7/Lesson7.HomeWork/Program.cs
@@ -59,14 +59,42 @@ char[] Duplicate(string str)
     string temp = "";
     for (int i = 0; i < str.Length - 1; i++)
     {
-        for (int j = i; j < str.Length - 1; j++)
+        for (int j = i + 1; j < str.Length; j++)
         {
-            if (str[j] != ' ' && str[i] == str[j])
+            if (str[j] != ' ' && str[i] == str[j] && !temp.Contains(str[i]))
             {
                 temp += str[i];
             }
         }
     }
     char[] arr = temp.ToCharArray();
+    arr.Distinct();
     return arr;
+}
+
+
+//перревірка
+Console.WriteLine("Завдання 1:");
+string str1 = "Hello";
+string str2 = "Hello";
+Console.WriteLine("Строка 1= " + str1 + "\t\tСтрока 2= " + str2);
+Console.WriteLine(Compare(str1, str2));
+
+Console.WriteLine("\nЗавдання 2:");
+string str3 = "123fght!@.,";
+Console.WriteLine("Строка= " + str3);
+(int chars, int digits, int specialCharacters) = Analize(str3);
+Console.WriteLine(chars + " " + digits + " " + specialCharacters);
+
+Console.WriteLine("\nЗавдання 3:");
+string str4 = "Hello and welcome";
+Console.WriteLine("Строка= " + str4);
+Console.WriteLine(Sort(str4));
+
+Console.WriteLine("\nЗавдання 4");
+char[] arr = Duplicate(str4);
+Console.WriteLine("Строка= " + str4);
+foreach (char word in arr)
+{
+    Console.Write(word);
 }
