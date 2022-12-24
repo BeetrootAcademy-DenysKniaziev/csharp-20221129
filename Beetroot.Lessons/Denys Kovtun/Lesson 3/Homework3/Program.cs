7833b6c67819100f2f2058850cf3ab996b47d722
KoD1407 <119446178+KoD1407@users.noreply.github.com>
2022-12-24 16:51:30 +0200

    Update Program.cs

--- Beetroot.Lessons/Denys Kovtun/Lesson 3/Homework3/Program.cs
+++ Beetroot.Lessons/Denys Kovtun/Lesson 3/Homework3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Введіть значення х:");
+int x = int.Parse(Console.ReadLine());
+
+Console.Write("Введіть значення y:");
+int y = int.Parse(Console.ReadLine());
+
+int sum = Math.Min(x, y);
+int i = Math.Min(x, y);
+
+while (i != Math.Max(x, y))
+{
+    i++;
+    sum += i;
+};
+
+Console.Write($"Сумма= {sum}");
