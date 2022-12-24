ebe068e327659c8ad301ce5bc6483e2dd23ee0ea
SkyLineR1001 <119454635+SkyLineR1001@users.noreply.github.com>
2022-12-24 12:41:47 +0200

    Work with String class

--- Serhii Rubayko/Lesson7.Classwork/Program.cs
+++ Serhii Rubayko/Lesson7.Classwork/Program.cs
@@ -1,2 +1,8 @@
 ﻿string str2 = new string('c', 3);
 Console.WriteLine(str2);
+
+string string1 = "it's hard day ";
+
+string1 += "night";
+
+Console.WriteLine(string1);
--- Serhii Rubayko/Lesson7.Homework/Program.cs
+++ Serhii Rubayko/Lesson7.Homework/Program.cs
@@ -1,16 +1,23 @@
-﻿class String
+﻿class Sring
 {
+    static bool SringEqual(string word1, string word2)
+    {
+        if (word1==word2) 
+            return true;
+        return false;
+    }
+
+      
 
 
+    static void Main()
+    {
+        var c = "alfa";
+        var d = "alfa";
+
+        Console.WriteLine(SringEqual(c, d));
 
 
-
-
+    }
 }
 
-static  Main()
-{
-
-
-
-}
