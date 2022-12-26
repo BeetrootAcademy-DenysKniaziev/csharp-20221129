004771fb658e35f826dfb796f50e974ec0c8e0db
Roman Bychkov <roman.bychkov.1921@gmail.com>
2022-12-26 03:06:21 +0200

    Roman Bychkov HomeWork 11

--- Roman Bychkov/Lesson11/Lesson11.HomeWork/Program.cs
+++ Roman Bychkov/Lesson11/Lesson11.HomeWork/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 
 class Program
 {
@@ -37,12 +38,27 @@ class Teacher
 
     public Subject[] Subjects { get; }
 
+    public int CountOfSubjects
+    {
+        get
+        {
+            var count = 0;
+            foreach (var subject in Subjects)
+            {
+                if (subject != null)
+                    count++;
+            }
+            return count;
+        }
+    }
+
+
     public Teacher(string name, string lastName, long phoneNumber)
     {
         Name = name;
         LastName = lastName;
         PhoneNumber = phoneNumber;
-        Subjects = new Subject[5];
+        Subjects = new Subject[3];
     }
     public void AddSubject(Subject newSubject)
     {
@@ -73,36 +89,35 @@ class Class
 {
     public string Name { get; set; }
 
-    public int CountOfPupil { get; set; }
+    public int CountOfPupils
+    {
+        get
+        {
+            return Pupils.Count();
+        }
+    }
     public Pupil[] Pupils { get; private set; }
     public Class(string name)
     {
         Name = name;
-        //Pupils = new Pupil[CountOfPupil];
+        Pupils = new Pupil[0];
     }
     public void AddPupil(Pupil pupil)
     {
-        if (Pupils == null)
-        {
-            Pupils = new Pupil[1];
-            Pupils[0] = pupil;
-        }
-        else
-        {
-            var newPupils = new Pupil[Pupils.Length + 1];
-            Array.Copy(Pupils, newPupils, Pupils.Length);
-            Pupils = newPupils;
-            Pupils[Pupils.Length - 1] = pupil;
-        }
+        var newPupils = new Pupil[Pupils.Length + 1];
+        Array.Copy(Pupils, newPupils, Pupils.Length);
+        Pupils = newPupils;
+        Pupils[Pupils.Length - 1] = pupil;
+
     }
     /// <summary>
     /// Return true if Remove was completed
     /// </summary>
-    public bool RemovePupil(ref Pupil pupil)
+    public bool RemovePupil(Pupil pupil)
     {
-        if(Pupils.Contains(pupil))
+        if (Pupils.Contains(pupil))
         {
-            int id = Array.IndexOf(Pupils,pupil);
+            int id = Array.IndexOf(Pupils, pupil);
             var newPupils = new Pupil[Pupils.Length - 1];
             Array.Copy(Pupils, 0, newPupils, 0, id);
             Array.Copy(Pupils, id + 1, newPupils, id, Pupils.Length - id - 1);
@@ -131,10 +146,36 @@ class Subject
     {
         Plan.Append(text);
     }
+    public override string ToString()
+    {
+        return Name;
+    }
 
 }
 
-class Schedule
+static class Schedule
 {
+    public static (DayOfWeek day, int hour, int minute, int classroom, Teacher Teacher, Subject Subject, Class myClass)[] Records { get; private set; }
+    static Schedule()
+    {
+        Records = new (DayOfWeek day, int hour, int minute, int classroom, Teacher Teacher, Subject Subject, Class myClass)[0];
+    }
+    public static bool Add(DayOfWeek day, int hour, int minute, int classroom, Teacher teacher, Subject subject, Class myClass)
+    {
+        if (!teacher.Subjects.Contains(subject))
+        {
+            Console.WriteLine("This teacher does not teach the given subject.");
+            return false;
+        }
+        var newRecords = new (DayOfWeek day, int hour, int minute, int classroom, Teacher Teacher, Subject Subject, Class myClass)[Records.Length + 1];
+        Array.Copy(Records, newRecords, Records.Length);
+        Records = newRecords;
+        Records[Records.Length - 1] = (day, hour, minute, classroom, teacher, subject, myClass);
+        return true;
+    }
 
+    static void ClearSchedule()
+    {
+        Records = null;
+    }
 }
