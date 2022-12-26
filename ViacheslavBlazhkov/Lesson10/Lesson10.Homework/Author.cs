fa0976f487552f3b7e00819a92a0530dfb29c0dd
Viacheslav Blazhkov <blazkov253@gmail.com>
2022-12-26 12:23:30 +0200

    homework 10

---
+++ ViacheslavBlazhkov/Lesson10/Lesson10.Homework/Author.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10.Homework
+{
+    internal class Author : Person
+    {
+        public Author() : base() { }
+        public Author(string fullName, string country, int birthYear) : base(fullName, country, birthYear) { }
+
+        public static Book WriteBook(string title, Author author)
+		{
+			return new Book(title, author);
+		}
+        public override string ToString()
+        {
+			return FullName;
+        }
+    }
+}
---
+++ ViacheslavBlazhkov/Lesson10/Lesson10.Homework/Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10.Homework
+{
+    internal class Book
+    {
+		private string _title;
+		public string Title
+		{
+			get { return _title; }
+			set { _title = value; }
+		}
+
+		private Author _author;
+		public Author Author
+		{
+			get { return _author; }
+			set { _author = value; }
+		}
+
+		private PublishingHouse _publishingHouse;
+		public PublishingHouse PublishingHouse
+		{
+			get { return _publishingHouse; }
+			set { _publishingHouse = value; }
+		}
+
+		private double _price;
+		public double Price
+		{
+			get { return _price; }
+			set { _price = value; }
+		}
+
+		public Book(string title, Author author) // when author write new book
+		{
+			_title = title;
+			_author = author;
+		}
+        public Book(string title, Author author, PublishingHouse pubHouse, double price) // when pubHouse publish new book
+        {
+            _title = title;
+            _author = author;
+			_publishingHouse = pubHouse;
+			_price = price;
+        }
+    }
+}
