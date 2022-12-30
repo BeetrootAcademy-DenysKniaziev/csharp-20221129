6eff472877d153d9ec1f7c9b70b4de4b662133d7
Roman Bychkov <roman.bychkov.1921@gmail.com>
2022-12-30 19:43:50 +0200

    Roman Bychkov HomeWork12

--- Roman Bychkov/Lesson12/Lesson12.HomeWork/Program.cs
+++ Roman Bychkov/Lesson12/Lesson12.HomeWork/Program.cs
@@ -40,19 +40,7 @@ class Part
         }
     }
 
-    public bool Sale(int count)
-    {
-        if (Count < count)
-            return false;
-        Count -= count;
-        return true;
-    }
-    public void Add(int count)
-    {
-        if (count < 0)
-            throw new ArgumentException("Count can't be less than 0.");
-        Count++;
-    }
+
     protected Part(string name, decimal price, int count, double weight)
     {
         Model = name;
@@ -64,16 +52,40 @@ class Part
 
 class Stock
 {
-    public Part[] Parts { get; set; }
-    public Stock()
+    public Part[] Parts { get; private set; }
+    public decimal Balance { get; set; }
+    public Stock(decimal balance = 0)
     {
+        Balance = balance;
         Parts = new Part[0];
+    }
+
+    public bool Sale(Part part, int count)
+    {
+        if (Parts.Contains(part) && part.Count >= count)
+        {
+            Balance += count * part.Price;
+            part.Count -= count;
+            return true;
+        }
+        return false;
+    }
+    public void AddPart(Part newPart)
+    {
+        if (!Parts.Contains(newPart))
+        {
+            var newParts = new Part[Parts.Length + 1];
+            Array.Copy(Parts, newParts, Parts.Length);
+            Parts = newParts;
+            Parts[Parts.Length - 1] = newPart;
+        }
     }
 }
 
 class Wheel : Part
 {
     double _radius;
+    string Type { get; set; }
     public double Radius
     {
         get => _radius;
@@ -84,9 +96,10 @@ class Wheel : Part
             _radius = value;
         }
     }
-    public Wheel(string name, decimal price, int count, double weight, double radius) : base(name, price, count, weight)
+    public Wheel(string name, decimal price, int count, double weight, double radius, string type) : base(name, price, count, weight)
     {
         Radius = radius;
+        Type = type;
     }
 }
 class Engine : Part
@@ -113,12 +126,12 @@ class Engine : Part
         }
     }
     public Engine(string name, decimal price, int count, double weight, double compression, double power) : base(name, price, count, weight)
-    { 
+    {
         Compression = compression;
         Power = power;
     }
 }
-class FuelTank: Part
+class FuelTank : Part
 {
     double _volume;
     public double Volume
@@ -132,7 +145,7 @@ class FuelTank: Part
         }
     }
     public FuelTank(string name, decimal price, int count, double weight, double volume) : base(name, price, count, weight)
-    { 
+    {
         Volume = volume;
     }
 }
