1c9f4c126008a53062ab4118ad9707a0010443ae
Roman Bychkov <roman.bychkov.1921@gmail.com>
2022-12-31 02:54:01 +0200

    Initial Commit

---
+++ Roman Bychkov/Lesson13/Lesson13.HomeWork/ProductAbstract.cs
@@ -0,0 +1,24 @@
+﻿abstract class Product
+{
+    protected decimal _price;
+
+    public string Name { get; set; }
+    public string Color { get; set; }
+
+    public decimal Price
+    {
+        get => _price;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentOutOfRangeException(nameof(value), "Value can't be less than 0");
+            _price = value;
+        }
+    }
+    protected Product(string name, decimal price, string color)
+    {
+        Name = name;
+        Price = price;
+        Color = color;
+    }
+}
---
+++ Roman Bychkov/Lesson13/Lesson13.HomeWork/Products/Clothing.cs
@@ -0,0 +1,50 @@
+﻿class Clothing : Product
+{
+    Dictionary<float, uint> _items;
+
+    Season Season { get; set; }
+
+    public Clothing(string name, decimal price, Season season, string color) : base(name, price, color)
+    {
+        _items = new Dictionary<float, uint>();
+        Season = season;
+    }
+    public void AddSize(float size, uint count = 0)
+    {
+        if (count < 0)
+            throw new ArgumentOutOfRangeException(nameof(count), "Count can't be less then 0.");
+        if (!_items.TryAdd(size, count))
+            throw new InvalidOperationException("This size contains in the list.");
+    }
+    public bool RemoveSize(float size)
+    {
+        if (_items.ContainsKey(size))
+        {
+            _items.Remove(size);
+            return true;
+        }
+        return false;
+    }
+    public bool AddCountToSize(float size, uint count)
+    {
+        if (_items.ContainsKey(size))
+        {
+            _items[size] += count;
+            return true;
+        }
+        else
+            throw new InvalidOperationException("This size contains in the list.");
+        return false;
+    }
+    public bool DeleteCountFromSize(float size, uint count)
+    {
+        if (_items.ContainsKey(size))
+        {
+            _items[size] -= count;
+            return true;
+        }
+        else
+            throw new InvalidOperationException("This size contains in the list.");
+        return false;
+    }
+}
