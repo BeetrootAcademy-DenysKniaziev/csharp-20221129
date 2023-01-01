e61c2823b2cde5e112bad2a0190c4ea4cd044917
Viacheslav Blazhkov <blazkov253@gmail.com>
2023-01-01 21:21:24 +0200

    lesson 11

---
+++ ViacheslavBlazhkov/Lesson11/Lesson11.Homework/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson11.Homework
+{
+    internal class Person
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
---
+++ ViacheslavBlazhkov/Lesson11/Lesson11.Homework/Subject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson11.Homework
+{
+    internal class Subject
+    {
+		private string _title;
+
+        public string Title
+		{
+			get { return _title; }
+			set { _title = value; }
+		}
+
+		public Subject(string title)
+		{
+			_title = title;
+		}
+	}
+}
