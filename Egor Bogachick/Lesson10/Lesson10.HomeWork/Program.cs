6f5f90be91547a67a22d629175b503dc9c3899e9
kovasnik <egorbf2003@gmail.com>
2023-01-03 22:16:07 +0200

    Egro Bogachik. Home work 10

--- Egor Bogachick/Lesson10/Lesson10.HomeWork/Program.cs
+++ Egor Bogachick/Lesson10/Lesson10.HomeWork/Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Xml.Linq;
+
+class Library
+{
+    public const string status = "Open";
+
+    public string Name { get; set; }
+    public Location Location { get; set; }
+    public Employee Employee { get; set; }
+    public Bookshelf Bookshelf { get; set; }
+
+    public static string GetStatus()
+    {
+        return "Library " + status;
+    }
+}
+
+class Location
+{
+    public string Country { get; set; }
+    public string Street { get; set; }
+}
+
+class Employee
+{
+    public const string DefaultMessage = "Need a worker";
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public int WorkExperience { get; set; }
+    public string FullName => FirstName + " " + LastName;
+
+    public Employee()
+    {
+        Console.WriteLine(DefaultMessage);
+    }
+    
+    public Employee(string firstName, string lastName)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+    }
+    public Employee(string firstName, string lastName, int age)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Age = age;
+    }
+
+}
+
+class Bookshelf
+{
+    public int Number { get; set; }
+    public string Category { get; set; }
+    public Book Book { get; set; }
+}
+
+class Book
+{
+    public string BookName { get; set; }
+    public string Author { get; set; }
+    public int NumberOfPages { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+
+    }
+}
