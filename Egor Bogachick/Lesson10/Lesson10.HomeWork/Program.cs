5d197c6f898e30b6c25c70b9d5d5a3e54cd1777b
kovasnik <egorbf2003@gmail.com>
2023-01-04 16:59:19 +0200

    Egor Bogachik. Home work 10

--- Egor Bogachick/Lesson10/Lesson10.HomeWork/Program.cs
+++ Egor Bogachick/Lesson10/Lesson10.HomeWork/Program.cs
@@ -19,6 +19,12 @@ class Location
 {
     public string Country { get; set; }
     public string Street { get; set; }
+
+    public Location(string country, string street)
+    {
+        this.Country = country;
+        this.Street = street;
+    }
 }
 
 class Employee
@@ -53,6 +59,7 @@ class Bookshelf
 {
     public int Number { get; set; }
     public string Category { get; set; }
+    public int NumberOfBooks { get; set; }
     public Book Book { get; set; }
 }
 
@@ -61,12 +68,45 @@ class Book
     public string BookName { get; set; }
     public string Author { get; set; }
     public int NumberOfPages { get; set; }
+
+    public override string ToString()
+    {
+        return "Book: " + BookName + "\nAuthor: " + Author + "\nNumber of pages: " + NumberOfPages;
+    }
 }
 
 class Program
 {
     static void Main()
     {
+        var book1 = new Book
+        {
+            BookName = "1984",
+            Author = "George Orwell",
+            NumberOfPages = 200
+        };
 
+        var bookshelf1 = new Bookshelf
+        {
+            Number = 1,
+            Category = "Novel",
+            NumberOfBooks = 5,
+            Book = book1
+        };
+
+        var employee1 = new Employee("Egor", "Bogachik", 19);
+
+        var location1 = new Location("Ukraine", "Shevchenko");
+        var lib1 = new Library
+        {
+            Name = "Royal library",
+            Location = location1,
+            Employee = employee1,
+            Bookshelf = bookshelf1,
+        };
+
+
+        Console.WriteLine(Library.GetStatus());
+        Console.WriteLine(book1);
     }
 }
