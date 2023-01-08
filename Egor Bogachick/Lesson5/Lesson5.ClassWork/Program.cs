6a8b97c5fd014a3c898a5bd0e49819978b2271b6
Egor Bogachik <79566284+kovasnik@users.noreply.github.com>
2023-01-08 15:19:49 +0200

    Egor Bogachik. Homework 5 (#74)

---
+++ Egor Bogachick/Lesson5/Lesson5.ClassWork/Program.cs
@@ -0,0 +1,12 @@
+﻿sbyte b = 100;
+int i = 1000;
+ushort s = checked((ushort)i);
+
+long ul = long.MaxValue - 512;
+double d = ul;
+ul = (long)d;
+
+char c = 'h';
+d = (int)c;
+
+Console.WriteLine(d);
