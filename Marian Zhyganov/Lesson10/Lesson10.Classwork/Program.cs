edc5a89e6029eb7189a4721c6ad4c6eb4c50e231
Zhyganov Marian <91560187+Marian3052@users.noreply.github.com>
2023-01-09 00:44:29 +0200

    MarianZyganov-Lesson6 (#90)
    Co-authored-by: Marian <marianzoganov09@gmail.com>

---
+++ Marian Zhyganov/Lesson10/Lesson10.Classwork/Program.cs
@@ -0,0 +1,15 @@
+﻿class Pesrson
+{
+    public string _name;
+}
+
+class Program
+{
+
+    static void Main()
+    {
+        var p1  = new Pesrson();
+        p1._name = "Maks";
+        Console.WriteLine(p1._name);
+    }
+}
