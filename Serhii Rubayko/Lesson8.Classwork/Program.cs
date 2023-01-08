23e20c9b18383d6a812a9707fbff283371172523
SkyLineR1001 <119454635+SkyLineR1001@users.noreply.github.com>
2023-01-08 20:14:16 +0200

    PhoneBookMain

--- Serhii Rubayko/Lesson8.Classwork/Program.cs
+++ Serhii Rubayko/Lesson8.Classwork/Program.cs
@@ -1,13 +1,13 @@
 ﻿using System.Text.RegularExpressions;
 
-////var regex = new Regex(@"^\d+$");
+var regex = new Regex(@"^\d+$");
 
-////var srt1 = "65675746";
+var srt1 = "65675746";
 
-////var str2 = "545grtg678";
+var str2 = "545grtg678";
 
-////Console.WriteLine(regex.IsMatch(srt1));
-////Console.WriteLine(regex.IsMatch(str2));
+Console.WriteLine(regex.IsMatch(srt1));
+Console.WriteLine(regex.IsMatch(str2));
 
 Console.WriteLine(Regex.IsMatch("458-435-2318", "[0-9]{3}-[0-9]{3}-[0-9]{4}"));
 var res = Regex.Match("458-435-2318", "[0-9]{3}-[0-9]{3}-[0-9]{4}");
@@ -18,70 +18,70 @@ Console.WriteLine();
 
 var resCol = Regex.Matches("458-435-2318, 785-114-2244, 987-654-7785, 7865-4588-77", @"\d{3}-\d{3}-\d{4}");
 
-//Console.WriteLine(resCol[1]);
+Console.WriteLine(resCol[1]);
 
 foreach (var col in resCol)
 {
     Console.WriteLine(col);
 }
 
-//var str3 = "458-435-2318, 785-114-2244, 987-654-7785, tijhyfutdyt";
-//var str4 = Regex.Replace(str3, @"\d{3}-\d{3}-\d{4}|\w+", "");
-//Console.WriteLine(str4);
+var str3 = "458-435-2318, 785-114-2244, 987-654-7785, tijhyfutdyt";
+var str4 = Regex.Replace(str3, @"\d{3}-\d{3}-\d{4}|\w+", "");
+Console.WriteLine(str4);
 
-//using System.IO;
+using System.IO;
 
-//foreach (var drive in DriveInfo.GetDrives())
-//{
-//    Console.WriteLine(drive.Name);
-//    Console.WriteLine(drive.TotalSize);
-//    Console.WriteLine(drive.AvailableFreeSpace);
-//}
+foreach (var drive in DriveInfo.GetDrives())
+{
+    Console.WriteLine(drive.Name);
+    Console.WriteLine(drive.TotalSize);
+    Console.WriteLine(drive.AvailableFreeSpace);
+}
 
-//string root = @"D:\";
-//if(Directory.Exists(root+"BeetRoot"))
-//{
-//    foreach (var subDirs in Directory.GetDirectories(root+ "BeetRoot"))
-//    {
-//        Console.WriteLine(subDirs);
-//    }
+string root = @"D:\";
+if (Directory.Exists(root + "BeetRoot"))
+{
+    foreach (var subDirs in Directory.GetDirectories(root + "BeetRoot"))
+    {
+        Console.WriteLine(subDirs);
+    }
 
-//    foreach (var files in Directory.GetDirectories(root + "BeetRoot"))
-//    {
-//        Console.WriteLine(files);
-//    }
-//}
-//    var dir = new DirectoryInfo(@"D:\Навчання");
-//if (dir.Exists)
-//{
-//    foreach (var subDir in dir.GetDirectories())
-//    {
-//        Console.WriteLine(subDir);
-//    }
-//    foreach (var file in dir.GetFiles())
-//    {
-//        Console.WriteLine(file);
-//    }
-//}
+    foreach (var files in Directory.GetDirectories(root + "BeetRoot"))
+    {
+        Console.WriteLine(files);
+    }
+}
+var dir = new DirectoryInfo(@"D:\Навчання");
+if (dir.Exists)
+{
+    foreach (var subDir in dir.GetDirectories())
+    {
+        Console.WriteLine(subDir);
+    }
+    foreach (var file in dir.GetFiles())
+    {
+        Console.WriteLine(file);
+    }
+}
 
-//var fileName = "testfile.txt";
-//if(File.Exists(fileName))
-//{
-//    Console.WriteLine(File.ReadAllText(fileName));
-//    File.AppendAllText(fileName, "\nsome more text");
-//}
-//else
-//{
-//    File.WriteAllText(fileName, "some else");
-//}
+var fileName = "testfile.txt";
+if (File.Exists(fileName))
+{
+    Console.WriteLine(File.ReadAllText(fileName));
+    File.AppendAllText(fileName, "\nsome more text");
+}
+else
+{
+    File.WriteAllText(fileName, "some else");
+}
 
-//var file = new FileInfo("testfile.txt");
-//if (File.Exists(fileName))
-//{
-//    Console.WriteLine(File.OpenText().ReadToEnd());
-//    File.AppendAllText(fileName, "\nsome more text");
-//}
-//else
-//{
-//    File.WriteAllText(fileName, "some else");
+var file = new FileInfo("testfile.txt");
+if (File.Exists(fileName))
+{
+    Console.WriteLine(File.OpenText().ReadToEnd());
+    File.AppendAllText(fileName, "\nsome more text");
+}
+else
+{
+    File.WriteAllText(fileName, "some else");
 //}
--- Serhii Rubayko/Lesson8.PhoneBook/Program.cs
+++ Serhii Rubayko/Lesson8.PhoneBook/Program.cs
@@ -1,99 +1,192 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 
 const string FileName = "PhoneBook.txt";
 
- Console.WriteLine("Phone Book Aplications");
-Console.WriteLine("Select Action:");
-Console.WriteLine("1 - Show Phone Book");
-Console.WriteLine("2 - Add Record");
-Console.WriteLine("3 - Update Record");
-Console.WriteLine("4 - Remove Record");
-Console.WriteLine("5 - Search Name");
-Console.WriteLine("0 - Exit");
+Console.WriteLine("Phone Book Aplications");
 
-//var records = new(string firstName, string lastName, string number)[1000];
-//records[0] = ("Serhii", "Rubayko", "343-958-4231");
-//SaveToFile(records);
-    
-var records = ReadFromFile();
+StartProgram();
 
-
-while (true) 
+void StartProgram()
 {
-    var action = Console.ReadKey();
-    
-    
-    Console.WriteLine();
-
-    switch (action.Key)
-    {
-        case ConsoleKey.NumPad1:
-        case ConsoleKey.D1:
-            Console.WriteLine("Phone Book:");
-            ShowPhoneBook(records);
-            break;
-        case ConsoleKey.D2:
-        case ConsoleKey.NumPad2:
-            AddRecord();
-            break;
-        case ConsoleKey.NumPad5:
-        case ConsoleKey.D5:
-            //Console.WriteLine("Found name:");
-            Search(records);
-            break;
-        case ConsoleKey.D0:
-        case ConsoleKey.NumPad0:
-            Console.WriteLine("Exit");
-            Environment.Exit(0);
-            break;
-        default:
-            Console.WriteLine("Incorrect Input!");
-            break;
-
-
-    }
+    Console.WriteLine("Select Action:");
+    Console.WriteLine("1 - Show Phone Book");
+    Console.WriteLine("2 - Add Record");
+    Console.WriteLine("3 - Update Record");
+    Console.WriteLine("4 - Remove Record");
+    Console.WriteLine("5 - Search Name");
+    Console.WriteLine("0 - Exit");
 }
+    //var records = new(string firstName, string lastName, string number)[1000];
+    //records[0] = ("Serhii", "Rubayko", "343-958-4231");
+    //SaveToFile(records);
+
+    var records = ReadFromFile();
 
 
+    while (true)
+    {
+        var action = Console.ReadKey();
 
-//ShowPhoneBook(records);
+
+        Console.WriteLine();
+
+        switch (action.Key)
+        {
+            case ConsoleKey.NumPad1:
+            case ConsoleKey.D1:
+                Console.WriteLine("Phone Book:");
+                ShowPhoneBook(records);
+                StartProgram();
+                break;
+            case ConsoleKey.D2:
+            case ConsoleKey.NumPad2:
+                AddRecord();
+                break;
+        case ConsoleKey.D3:
+        case ConsoleKey.NumPad3:
+            UpdateRecord();
+            break;
+        case ConsoleKey.D4:
+            case ConsoleKey.NumPad4:
+                RemoveRecord();
+            break;
+            case ConsoleKey.NumPad5:
+            case ConsoleKey.D5:
+                Search(records);
+                break;
+            case ConsoleKey.D0:
+            case ConsoleKey.NumPad0:
+                Console.WriteLine("Exit");
+                Environment.Exit(0);
+                break;
+            default:
+                Console.WriteLine("Incorrect Input!");
+                break;
+
+
+        }
+    }
+
+
 
 void ShowPhoneBook((string firstName, string lastName, string number)[] records)
 {
+    var index = 1;
     foreach (var record in records)
     {
-        Console.WriteLine($"First Name: {record.firstName}, Last Name: {record.lastName}, Number: {record.number}");
+        Console.WriteLine($"{index}".PadRight(4)+$"First Name: {record.firstName}, Last Name: {record.lastName}, Number: {record.number}");
+        index += 1;
     }
+    Console.WriteLine();
+    //StartProgram();
 }
 
 void Search((string firstName, string lastName, string number)[] records)
 {
-    Console.WriteLine("Input FirstName:");
+    Console.WriteLine("Input search type: \n 1 - Search by FirstName \n 2 - Search by LastName \n 3 - Search by Number");
 
-    var search= Console.ReadLine();
+    var type= Console.ReadKey();
 
-    var people = new string[records.Length / 2];
-
-    for (int i=0; i<records.Length; i++)
+    switch(type.Key)
     {
-        var record = records[i];
-        
-        if (search == record.firstName)
+        case ConsoleKey.NumPad1:
+        case ConsoleKey.D1:
+            Console.WriteLine("\nInput search argument - FirstName (fullname or some initial characters) :");
+            string search = Console.ReadLine();
+            SearchFistName(search, records);
+            StartProgram();
+            break;
+        case ConsoleKey.NumPad2:
+        case ConsoleKey.D2:
+            Console.WriteLine("\nInput search argument - LastName (fullname or some initial characters) :");
+            search = Console.ReadLine();
+            SearchLastName(search, records);
+            StartProgram();
+            break;
+        case ConsoleKey.NumPad3:
+        case ConsoleKey.D3:
+            Console.WriteLine("\nInput search argument - Number (fullnumber or some initial characters) :");
+            search = Console.ReadLine();
+            SearchNumber(search, records);
+            StartProgram();
+            break;
+        default:
+            Console.WriteLine("Incorrect Input!");
+            StartProgram();
+            break;
+
+    }
+    
+    //string item ="Item1";
+    void SearchFistName(string search, (string firstName, string lastName, string number)[] records)
+    {
+        if (search != null)
         {
-            //var found = records[i];
-            int j = 0;
-            people[j] = $"First Name: {record.firstName}, Last Name: {record.lastName}, Number: {record.number}";
-            j++;
-        }
+            int counter = 0;
+            for (int i = 0; i < records.Length; i++)
+            {
+                //var record = records[i];
 
-        //Console.WriteLine("Incorrect Input!");
+                if (Regex.IsMatch(records[i].firstName, $"^{search}"))
+                {
+                    counter += 1;
+                    Console.WriteLine($"First Name: {records[i].firstName}, Last Name: {records[i].lastName}, Number: {records[i].number}");
+                }
+            }
+            if (counter == 0)
+            {
+                Console.WriteLine("No matches found, input <5> for another try");
+            }
+            Console.WriteLine();
+        }       
     }
 
-    foreach (var man in people)
+    void SearchLastName(string search, (string firstName, string lastName, string number)[] records)
     {
-        Console.WriteLine(man);
+        if (search != null)
+        {
+            int counter = 0;
+            for (int i = 0; i < records.Length; i++)
+            {
+                //var record = records[i];
+
+                if (Regex.IsMatch(records[i].lastName, $"^{search}"))
+                {
+                    counter += 1;
+                    Console.WriteLine($"First Name: {records[i].firstName}, Last Name: {records[i].lastName}, Number: {records[i].number}");
+                }
+            }
+            if (counter == 0)
+            {
+                Console.WriteLine("No matches found, input <5> for another try");
+            }
+            Console.WriteLine();
+        }
     }
-        
+
+    void SearchNumber(string search, (string firstName, string lastName, string number)[] records)
+    {
+        if (search != null)
+        {
+            int counter = 0;
+            for (int i = 0; i < records.Length; i++)
+            {
+                //var record = records[i];
+
+                if (Regex.IsMatch(records[i].number, $"^{search}"))
+                {
+                    counter += 1;
+                    Console.WriteLine($"First Name: {records[i].firstName}, Last Name: {records[i].lastName}, Number: {records[i].number}");
+                }
+            }
+            if (counter == 0)
+            {
+                Console.WriteLine("No matches found, input <5> for another try");
+            }
+            Console.WriteLine();
+        }
+    }
 }
 
 void AddRecord()
@@ -111,7 +204,7 @@ void AddRecord()
         while (!match)
         {
             Console.WriteLine("Incorrect format, input newRecord:");
-             newRecord = Console.ReadLine();
+            newRecord = Console.ReadLine();
             match = Regex.IsMatch(newRecord, pattern);
         }
     }  
@@ -130,18 +223,155 @@ void AddRecord()
     SaveToFile(recordNew);
 
     records = ReadFromFile();
-       
+
+    Console.WriteLine($"New Record {records[0]} was successfully added");
+    Console.WriteLine();
+
+    StartProgram();
 }
 
 void UpdateRecord()
 {
+    ShowPhoneBook(records);
 
+    string d;
+    int numb;
+    bool b;
+    do
+    {
+        Console.WriteLine($"Select number record for Update from 1 to {records.Length}");
+
+        d = Console.ReadLine();
+
+        b = Int32.TryParse(d, out numb);
+
+    }
+    while (!b || numb > records.Length || numb <= 0);
+
+    Console.WriteLine($"Do you want Update record \n {records[numb - 1]}? \nyes - y / no - anykey");
+
+    var record = records[numb - 1];
+
+    if (Regex.IsMatch(Console.ReadLine(), "y"))
+    {
+        Console.WriteLine("Input new data of records: (Firstname|Lastname|XXX-XXX-XXXX)");
+
+        var newRecord = Console.ReadLine();
+
+        var pattern = "<word>|<word>|[0-9]{3}-[0-9]{3}-[0-9]{4}";
+
+        bool match = Regex.IsMatch(newRecord, pattern);
+
+        if (!match)
+        {
+            while (!match)
+            {
+                Console.WriteLine("Incorrect format, input newRecord:");
+                newRecord = Console.ReadLine();
+                match = Regex.IsMatch(newRecord, pattern);
+            }
+        }
+
+        var splited = newRecord.Split('|');
+
+        var recordNew = new (string firstName, string lastName, string number)[records.Length];
+
+        if (numb > 1)
+        {
+            for (int i = 0; i < numb - 1; i++)
+            {
+                recordNew[i] = records[i];
+            }
+
+            recordNew[numb - 1] = (splited[0], splited[1], splited[2]);
+
+            for (int i = numb; i < recordNew.Length; i++)
+            {
+                recordNew[i] = records[i];
+            }
+        }
+        else if (numb == 1)
+        {
+            recordNew[0] = (splited[0], splited[1], splited[2]);
+
+            for (int i = numb; i < records.Length; i++)
+            {
+                recordNew[i] = records[i];
+            }
+        }
+        SaveToFile(recordNew);
+
+        records = ReadFromFile();
+
+        Console.WriteLine($"{record} was successfully updated to {records[numb - 1]}");
+        Console.WriteLine();
+
+        StartProgram();
+
+    }
+    else
+    {
+        StartProgram();
+    }
 }
+
 
 void RemoveRecord()
 {
+    ShowPhoneBook(records);
 
-}
+    string d;
+    int numb;
+    bool b;
+    do
+    {
+        Console.WriteLine($"Select number record for Remove from 1 to {records.Length}");
+
+        d = Console.ReadLine();
+
+        b = Int32.TryParse(d, out numb);
+
+    }
+    while (!b || numb > records.Length || numb <= 0);
+    
+        Console.WriteLine($"Do you want remove record \n {records[numb - 1]}? \nyes - y / no - anykey");
+          
+    if(Regex.IsMatch(Console.ReadLine(), "y"))
+    {
+        var record = records[numb - 1];
+        var recordNew = new (string firstName, string lastName, string number)[records.Length - 1];
+
+        if (numb > 1)
+        {
+            for (int i = 0; i < numb - 1; i++)
+            {
+                recordNew[i] = records[i];
+            }
+            for (int i = numb; i < records.Length; i++)
+            {
+                recordNew[i - 1] = records[i];
+            }
+        }
+        else if(numb==1)
+        {
+            for (int i = numb; i < records.Length; i++)
+            {
+                recordNew[i - 1] = records[i];
+            }
+        }
+        SaveToFile(recordNew);
+
+        records = ReadFromFile();
+
+        Console.WriteLine($"{record} was successfully removed");
+        Console.WriteLine();
+
+        StartProgram();
+    }
+
+    StartProgram();
+} 
+
 
 void SaveToFile((string firstName, string lastName, string number)[] records)
 {
