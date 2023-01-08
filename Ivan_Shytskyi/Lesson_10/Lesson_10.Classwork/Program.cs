f2dde85f3bde7b64595c556afb94df1bcb3c95da
shytskyi <111076900+shytskyi@users.noreply.github.com>
2023-01-08 23:07:30 +0100

    Shytskyi lesson 10 (#101)
    * Initial Commit classwork
    
    * 1
    
    * Classwork
    
    * 1
    
    * Initial Commit homework
    
    * 1
    
    * 2
    
    * 3
    
    * edit
    
    * edit_1

---
+++ Ivan_Shytskyi/Lesson_10/Lesson_10.Classwork/Program.cs
@@ -0,0 +1,111 @@
+﻿class Person
+{
+    public const string DefaultName = "Noname";
+    public const string DefaultLastName = "Somebody";
+
+    private string _name;
+
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    public string LastName { get; set; }
+
+    public string FullName => Name + " " + LastName;
+
+    public static int MaxAge = 100;
+
+    static Person()
+    {
+        Console.WriteLine("Static ctor of Person");
+    }
+
+    public Person()
+    {
+        Name = DefaultName;
+        LastName = DefaultLastName;
+    }
+
+    public Person(string name, string lastName)
+    {
+        Name = name;
+        LastName = lastName;
+    }
+
+    public Person(Person person)
+    {
+        Name = person.Name;
+        LastName = person.LastName;
+    }
+
+    public void SayHello()
+    {
+        Console.WriteLine("Hello!");
+    }
+    public void Say(string text)
+    {
+        Console.WriteLine(text);
+    }
+
+    private string ConvertToString()
+    {
+        return $"{this.FullName}, MaxAge={MaxAge}";
+    }
+
+    public override string ToString()
+    {
+        return ConvertToString();
+    }
+
+    public static string GetDefaultValues()
+    {
+        return DefaultName + " " + DefaultLastName;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var p1 = new Person();
+        p1.Name = "Denys";
+        p1.LastName = "Kniaziev";
+        //Console.WriteLine(p1.FullName);
+
+        //p1.SayHello();
+        //p1.Say("Hey!");
+
+        var p2 = new Person
+        {
+            Name = "Roman",
+            LastName = "Bychkov"
+        };
+        //Console.WriteLine(p2.FullName);
+
+        var p3 = new Person();
+        Console.WriteLine(p3.FullName);
+
+        var p4 = new Person("Marian", "Zhyganov");
+        Console.WriteLine(p4.FullName);
+
+        var p5 = new Person(p4);
+        Console.WriteLine(p5.FullName);
+
+        Console.WriteLine(Person.MaxAge);
+        Console.WriteLine(Person.DefaultLastName);
+        Person.MaxAge = 120;
+        Console.WriteLine(Person.MaxAge);
+
+        Console.WriteLine(p3);
+        Console.WriteLine(p4);
+        Console.WriteLine(p5);
+        Person.MaxAge = 150;
+        Console.WriteLine(p3);
+        Console.WriteLine(p4);
+        Console.WriteLine(p5);
+
+        Console.WriteLine(Person.GetDefaultValues());
+    }
+}
---
+++ Ivan_Shytskyi/Lesson_10/Lesson_10.Homework/Program.cs
@@ -0,0 +1,120 @@
+﻿class Location
+{
+    public string _address;
+    public Location(string Country, string City, string Street, string NumberOfHouse)
+    {
+        _address = $"{Country} {City} {Street} {NumberOfHouse}";
+    }
+}
+class Libraries
+{
+    public int _libraryNumber;
+    private int _numberOfAllBooks;
+    private string _location;
+    public string Location1
+    {
+        get { return _location; }
+        set { _location = value; }
+    }
+    public int LibraryNumber 
+    {
+        get { return _libraryNumber; } 
+        set { _libraryNumber = value; } 
+    }
+    public int NumberOfAllBooks
+    {
+        get { return _numberOfAllBooks; }
+        set { _numberOfAllBooks = value; }
+    }
+
+    
+    public Libraries (string Country, string City, string Street, string NumberOfHouse, int libraryNumber, int numberOfAllBooks)
+    {
+        Location a = new Location(Country, City, Street, NumberOfHouse);
+        Location1 = a._address;
+        LibraryNumber = libraryNumber;
+        NumberOfAllBooks = numberOfAllBooks;
+    }
+    public void Info()
+    {
+       Console.WriteLine($"Location library: {Location1}" +
+           $"\nLibrary number - {LibraryNumber}\nNumber Of all books - {NumberOfAllBooks}"); 
+    }
+    public void OpenClose()
+    {
+        DateTime dt = new DateTime();
+        dt = DateTime.Now;
+        TimeOnly t = new TimeOnly(9, 0);
+        TimeOnly t1 = new TimeOnly(17, 0);
+        TimeOnly t0 = new TimeOnly();
+        t0 = TimeOnly.FromDateTime(dt);
+
+        Console.WriteLine($"Time work librarie: {t} - {t1}");
+        if (t0 > t && t0 < t1)
+            Console.WriteLine("Libraries is open");
+        else
+            Console.WriteLine("Libraries is close");
+    }
+}
+class Author
+{
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public string DateOfBorn { get; set; }
+    public string DateOfDie { get; set; }
+    public string FullName => LastName + " " + FirstName;
+    public string DateOfBornAndDie => DateOfBorn + " - " + DateOfDie;
+}
+class Book
+{
+    public string BookLocation { get; set; }
+    public string Name { get; set; }
+    public Author Author { get; set; }
+    public int Year { get; set; }
+    public int NumberOfBooks { get; set; }
+    public Book (string name, Author author, int year, int numberOfBooks, string location)
+    {
+        Name = name;
+        Author = author;
+        Year = year;
+        NumberOfBooks = numberOfBooks;
+        BookLocation = location;
+    }
+    public void Available()
+    {
+        if (NumberOfBooks > 1)
+            Console.WriteLine($"{Name} is available");
+        else
+            Console.WriteLine($"{Name} is not available");
+    }
+    public void BookInfo()
+    {
+        Console.WriteLine($"Information of book: {Name}" +
+            $"\nAuthor - {Author.FullName} ({Author.DateOfBornAndDie})\nNumber Of books - {NumberOfBooks}\nYear - {Year}\n" +
+            $"Book location: {BookLocation}");
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        var l1 = new Libraries("uk", "london", "streat_1", "12", 1, 100);
+        Author author1 = new Author
+        {
+            LastName = "King",
+            FirstName = "Stephen",
+            DateOfBorn = "21.09.1947",
+            DateOfDie = "_"
+        };
+        Book book1 = new Book("The Green Mile", author1, 1996, 0, l1.Location1);
+        l1.OpenClose();
+        Console.WriteLine(new string ('*',20));
+        l1.Info();
+        Console.WriteLine(new string('*', 20));
+        book1.BookInfo();
+        Console.WriteLine(new string('*', 20));
+        book1.Available();
+        Console.WriteLine(new string('*', 20));
+    }
+}
+
