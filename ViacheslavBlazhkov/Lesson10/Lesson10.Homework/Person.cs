78925fa4b9d550b403909fcc2eccf7da4e67aa48
Viacheslav Blazhkov <79785720+ViacheslavBlazhkov@users.noreply.github.com>
2023-01-08 23:18:09 +0200

    ViacheslavBlazhkov.Lesson10 (#100)
    * Last commit
    
    * LC
    
    * commit
    
    * homework 9
    
    * homework 10
    
    Co-authored-by: Denys Kniaziev <denisknyazev96@gmail.com>

---
+++ ViacheslavBlazhkov/Lesson10/Lesson10.Homework/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10.Homework
+{
+    internal class Person
+    {
+        private string _fullName;
+        public string FullName
+        {
+            get { return _fullName; }
+            set { _fullName = value; }
+        }
+
+        private string _country;
+        public string Country
+        {
+            get { return _country; }
+            set { _country = value; }
+        }
+
+        private int _birthYear;
+        public int BirthYear
+        {
+            get { return _birthYear; }
+            set { _birthYear = value; }
+        }
+
+        public Person()
+        {
+            _fullName = "Unknown";
+            _country = "Unknown";
+            _birthYear = -1;
+        }
+        public Person(string fullName, string country, int birthYear)
+        {
+            _fullName = fullName;
+            _country = country;
+            _birthYear = birthYear;
+        }
+    }
+}
---
+++ ViacheslavBlazhkov/Lesson10/Lesson10.Homework/Program.cs
@@ -0,0 +1,13 @@
+﻿using Lesson10.Homework;
+
+Author auth1 = new Author("Markus Zusak", "Australia", 1975);
+Book book1 = Author.WriteBook("Book Thief", auth1);
+
+Console.WriteLine($"{book1.Author} has wrote {book1.Title}");
+
+PublishingHouse pubHouse1 = new PublishingHouse("FLC", "Lviv");
+book1 = PublishingHouse.PublishBook(book1, pubHouse1, 249.99);
+
+Console.WriteLine($"{book1.PublishingHouse} has publish {book1.Title}");
+
+Console.WriteLine($"First book: {book1.Title}|{book1.Author}|{book1.PublishingHouse}|{book1.Price}");
