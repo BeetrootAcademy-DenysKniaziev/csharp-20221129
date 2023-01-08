005051563ff23cd1cbcaae86703c83f212070e9d
Viacheslav Blazhkov <79785720+ViacheslavBlazhkov@users.noreply.github.com>
2023-01-08 15:58:44 +0200

    ViacheslavBlazhkov. Lesson 8 (#85)
    * Last commit
    
    * LC
    
    * commit

---
+++ ViacheslavBlazhkov/Lesson9/Lesson9.Classwork/Program.cs
@@ -0,0 +1,66 @@
+﻿try
+{
+    Method1();
+}
+//catch (DivideByZeroException ex)
+//{
+//    Console.WriteLine("DivideByZero");
+//}
+//catch (ArithmeticException ex)
+//{
+//    Console.WriteLine("ArithExcep!");
+//}
+catch (Exception ex)
+{
+    Console.WriteLine();
+    Console.WriteLine("Main Exp " + ex);
+    Console.WriteLine("Main Inner Exp" + ex.InnerException);
+}
+finally
+{
+    Console.WriteLine("Finally Main!");
+}
+
+Console.WriteLine("Next code...");
+
+void Method1()
+{
+    try
+    {
+        Method2();
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Method 1" + ex);
+        throw new Exception(ex.Message, ex);
+    }
+    finally { 
+        Console.WriteLine("Finally Method 1"); 
+    }
+}
+
+void Method2()
+{
+    try
+    {
+        //var a = 10;
+        //var b = 0;
+        //Console.WriteLine(a / b);
+
+        //File.OpenRead("dw");
+
+        //throw new Exception("General Exp!");
+        throw new ArithmeticException("ArithGeneral Exp!");
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Method 2 Catch " + ex);
+        throw ex;
+    }
+    finally
+    {
+        Console.WriteLine("Finally Method2");
+    }
+}
