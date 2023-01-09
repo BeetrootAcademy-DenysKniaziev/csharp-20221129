d57d55003825558684fd0c711ce9c63d1eebfc6f
KoD1407 <119446178+KoD1407@users.noreply.github.com>
2023-01-09 23:13:51 +0200

    Update Program.cs

--- Beetroot.Lessons/Denys Kovtun/Lesson 10/Homework 10/Program.cs
+++ Beetroot.Lessons/Denys Kovtun/Lesson 10/Homework 10/Program.cs
@@ -1,4 +1,23 @@
-﻿class Building
+class Writer
 {
-
+    public string Name { get; set; }
+    public Location Location { get; set; }
+}
+class Location
+{
+    public string Country { get; set; }
+    public string City { get; set; }
+}
+class Book
+{
+    public string Name { get; set; }
+    public Writer NameOfWriter { get; set; }
+    public int NumberOfPage { get; set; }
+}
+class Library
+{
+    public string Name { get; set; }
+    public Location Location { get; set; }
+    public int NumberOfBooks { get; set; }
+    public Book Book { get; set; }
 }
