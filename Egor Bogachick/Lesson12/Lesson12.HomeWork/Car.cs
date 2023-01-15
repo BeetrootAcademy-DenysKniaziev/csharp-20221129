9e225c2cc54fa6dbfdc09c5e528279d12d0fcfbe
Egor Bogachik <79566284+kovasnik@users.noreply.github.com>
2023-01-15 21:58:58 +0200

    Egor Bogachik. Home work 12 (#137)

---
+++ Egor Bogachick/Lesson12/Lesson12.HomeWork/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson12.HomeWork
+{
+    public class Car : IWorkable, IMovable
+    {
+        public string Brand { get; set; }
+        public string Date { get; set; }
+        public Engine Engine { get; set; }
+        public Wheel Wheel { get; set; }
+        public bool IsWorking { get; set; } = true;
+
+        public Car(string brand, string date)
+        {
+            this.Brand = brand;
+            this.Date = date;
+        }
+        public Car(string brand, string date, Engine engine, Wheel wheel)
+        {
+            this.Brand = brand;
+            this.Date = date;
+            this.Engine = engine;
+            this.Wheel = wheel;
+        }
+        public virtual void Move()
+        {
+            Console.WriteLine("Moves at a speed of 60 km");
+        }
+
+        public virtual void Status()
+        {
+            if (Engine == null || Wheel == null)
+            {
+                Console.WriteLine("The car is missing parts");
+            }
+            else if(Engine.IsWorking == false )
+            {
+                Engine.Status();
+            }
+            else if (Wheel.IsWorking == false)
+            {
+                Wheel.Status();
+            }
+            else if (IsWorking == false)
+            {
+                Console.WriteLine("Car is not ready");
+            }
+            else
+            {
+                Console.WriteLine("Car works fine");
+            }
+        }
+    }
+}
---
+++ Egor Bogachick/Lesson12/Lesson12.HomeWork/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Lesson12.HomeWork
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Engine engine1 = new Engine(12, 12314, 12000, false);
+            Engine engine2 = new Engine(11, 12000, 10000);
+            engine1.Status();
+            engine2.Status();
+            Wheel wheel1 = new Wheel("Sport", "12.07.12", false);
+            Wheel wheel2 = new Wheel("Common", "12.07.12");
+            wheel1.Status();
+            wheel2.Status();
+            Car car1 = new Car("Mercedes", "12.07.12", engine1, wheel2);
+            SportCar sportCar1 = new SportCar("Lamborghini", "12.07.18", engine2, wheel2);
+            BusinessCar businessCar = new BusinessCar("Something", "22.04.22");
+            car1.Status();
+            car1.Move();
+            sportCar1.Status();
+            sportCar1.Move();
+            businessCar.Status();
+            businessCar.Move();
+
+        }
+    }
+}
