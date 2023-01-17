113ee4f2e9433341290efd549d8e7cfdef0b782f
RomanNoname <105872125+RomanNoname@users.noreply.github.com>
2023-01-17 18:33:00 +0200

    RomanBychkov homework16 (#142)
    * Initial commit
    
    * Roman Bychkov homework16
    
    * Roman Bychkov homework16 v2

---
+++ Roman Bychkov/Lesson16/Lesson16.HomeWork/Voting.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+
+internal class Voting : IVote, IEnumerable<KeyValuePair<Option, int>>
+{
+    private Dictionary<Option, int> _options;
+    public string Name { get; set; }
+    public DateTime DateOfCreate { get; }
+    public Voting(string name, DateTime? dayOfCreate)
+    {
+        Name = name;
+        DateOfCreate = dayOfCreate is null ? DateTime.Today : dayOfCreate.Value;
+        _options = new Dictionary<Option, int>();
+    }
+    public void AddOption(string name)
+    {
+        var newObj = new Option(name);
+        if (!_options.ContainsKey(newObj))
+            _options.Add(newObj, 0);
+        else
+            Console.WriteLine("This option already exist.");
+    }
+    public void RemoveOption(string name)
+    {
+        _options.Remove(new Option(name));
+    }
+
+    public void MakeVote(Option option)
+    {
+        if (_options.ContainsKey(option))
+            _options[option]++;
+    }
+    public IEnumerator<KeyValuePair<Option, int>> GetEnumerator()
+    {
+        return _options.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Name, DateOfCreate.Year, DateOfCreate.Month, DateOfCreate.Day);
+    }
+    public override bool Equals(object? obj)
+    {
+        if (obj is Voting ob)
+            return ob.Name == Name && ob.DateOfCreate.Year == DateOfCreate.Year
+                && ob.DateOfCreate.Month == DateOfCreate.Month && ob.DateOfCreate.Day == DateOfCreate.Day;
+        return false;
+    }
+}
+struct Option
+{
+    //Can be more fields
+    private string _name;
+    public Option(string name)
+    {
+        _name = name;
+    }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(_name);
+    }
+    public override bool Equals(object? obj)
+    {
+        if (obj is Option ob)
+            return ob._name == _name;
+        return false;
+    }
+    public override string ToString()
+    {
+        return _name;
+    }
+}
