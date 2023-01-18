25d908262e3123e99da6ba88b349709efbdc7378
DmytroBonislavskyi <119468981+IMBomb@users.noreply.github.com>
2023-01-18 13:28:35 +0100

    Dmytro bonislavskyi dev (#128)
    * Lesson2 Homework
    
    * Request for X add
    
    * Homework for lesson3 added
    
    * Changed dirs for previus homeworks and add beta of RPG
    
    * Homework 5 added
    
    * Homework 5 added
    
    * Homework4
    
    * Homewor4 to Lesson4
    
    * Homework6 add
    
    * Homework 7 add
    
    * Homework8 added. Two types of serch implemented binary as homework task and simple one for some comfortable things
    
    * Homework9 added
    
    * Added Homework10 + UML file
    
    * Homework 11 Added
    
    * Homework12 Added + UML file
    
    * Homework13 add
    
    * Homework14 Add
    
    * Homework14 Add
    
    * Homework15 Added
    
    * Homework15 Readded

---
+++ Lesson14/Homework14/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Data.Common;
+using System.Drawing;
+
+namespace Homework14;
+
+public struct Point : IEquatable<Point>
+{
+    int _x, _y;
+    //bool _isChecked;
+    public int Level { get; set; }
+    public int X { get { return _x; } set { _x = value; } }
+    public int Y { get { return _y; } set { _y = value; } }
+    public Color PColor { get; set; }
+
+    public static bool operator ==(Point p1, Point p2)
+    {
+        decimal rDiv, gDiv, bDiv;
+        rDiv = Math.Abs(p1[0] - p2[0]);
+        gDiv = Math.Abs(p1[1] - p2[1]);
+        bDiv = Math.Abs(p1[2] - p2[2]);
+        if (rDiv + gDiv + bDiv > 75) return false;
+        return true;
+    }
+    public static bool operator !=(Point p1, Point p2)
+    {
+        return !(p1 == p2);
+    }
+
+    public bool Equals(Point other)
+    {
+        return this == other;
+    }
+
+    public int this[int index]
+    {
+        get
+        {
+            return index switch
+            {
+                0 => PColor.R,
+                1 => PColor.G,
+                2 => PColor.B,
+                3 => PColor.A,
+                _ => throw new InvalidOperationException("No atribute of this type"),
+            };
+
+        }
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.SetWindowSize(200, 60);
+        Picture Pic1 = new(), Pic2 = new(), Pic3 = new();
+        Pic1.LoadBitmap(AppDomain.CurrentDomain.BaseDirectory + "sword1s.jfif");
+        Console.WriteLine(Pic1.ToString());
+
+        Pic2.LoadBitmap(AppDomain.CurrentDomain.BaseDirectory + "bow2.jfif");
+        Console.WriteLine(Pic2.ToString());
+
+        Pic3.LoadBitmap("drag3.jfif");
+        Console.WriteLine(Pic3.ToString());
+    }
+}
---
+++ Lesson15/Homework15/Stack.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework15;
+
+public class Element<U>
+{
+    U _element;
+    public U TheElement { get {return _element ; } set {_element = value; } }
+    public Element<U> Next { get; set; }
+    public Element<U> Previeus { get; set; }
+    public Element(U obj)
+    {
+        _element = obj;
+    }
+    public Element()
+    {
+    }
+}
+
+internal class MyStack<U> //: IEnumerable<U>
+{
+    //public U Element { get; set; }
+    //public Element<U> CurrentElement { get; set; }
+    public Element<U> Top { get; set; }
+    public Element<U> Bottom { get; set; }
+
+    public void Push(U obj) //Push(obj) - adds obj at the top of stack
+    {
+        var temp = Top;
+        Top = new Element<U>(obj);
+        Top.Next = null;
+        if (Bottom != null)
+        {
+            temp.Next = Top;
+            Top.Previeus = temp;
+        }
+        else
+        {
+            Top.Previeus = null;
+            Bottom = Top;
+        }
+        Count++;
+    }
+
+    public U Pop() //Pop() -returns top element of stack & removes it
+    {
+        var temp = Top;
+        try
+        {
+
+            if (Top != null)
+            {
+                if (Top != Bottom)
+                {
+                    Top.Previeus.Next = null;
+                    Top = temp.Previeus;
+                    Count--;
+                }
+                else this.Clear();
+
+                return temp.TheElement;
+            }
+            else 
+            {
+                throw new Exception("No more elements in MyStack"); 
+            }
+        }
+        catch (Exception Ex)
+        {
+            Console.WriteLine(Ex);
+            return new Element<U>().TheElement;
+        }
+
+
+    }
+
+    public void Clear()//Clear() -clear stack
+    {
+        Count = 0;
+        Bottom = null;
+        Top = null;
+    }
+    public int Count { get; set; } //Count - property return number of elements
+    public U Peek() //Peek() -returns top element but doesn’t remove it
+    { 
+        if(Top != null) return Top.TheElement;
+        else
+        {
+            Console.WriteLine("No Elements in MyStack");
+            return new Element<U>().TheElement; 
+        }
+    }
+    public U[] CopyTo(U[] arr) //CopyTo(arr) -copies stack to array
+    {
+        Element<U> element = Bottom;
+        for (int i = 0; i < arr.Length; i++) 
+        {
+            if (element != null) { arr[i] = element.TheElement; }
+            else break;
+            element = element.Next;
+        }
+        return arr;
+    
+    }
+
+    //IEnumerator IEnumerable.GetEnumerator()
+    //{                                               
+    //    return ((IEnumerable)this).GetEnumerator();
+    //}
+
+    //IEnumerator<U> IEnumerable<U>.GetEnumerator()
+    //{
+    //    Element<U> temp = Bottom; 
+    //    while (temp != null) 
+    //    {
+    //        yield return temp.TheElement;
+    //        temp = temp.Next; 
+    //    }
+    //}
+
+
+
+}
