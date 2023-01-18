092476c017a85f724fa4d64487543b30dfca600d
Viacheslav Blazhkov <blazkov253@gmail.com>
2023-01-18 14:51:12 +0530

    commit

---
+++ ViacheslavBlazhkov/Lesson13/Lesson13.Homework/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Lesson13.Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product ball = new Product("Soccer Ball", 28.89, 5);
+            Product dinotoy = new Product("Dinosaur Toy", 22.8, 4);
+
+            Product[] productsList = new Product[] {ball, dinotoy};
+
+            User firstUser = new User("Oleksiy", "Ivankov", "283-293-8273");
+            User secondUser = new User("Nazar", "Galkovich", "382-123-8583");
+
+            firstUser.BuyProducts(secondUser, productsList);
+
+            Product.ShowInfo(ball, dinotoy);
+        }
+    }
+}
