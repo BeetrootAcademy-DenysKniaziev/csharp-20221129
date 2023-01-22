055238132a675e3536ca7679aaa36cd2804be706
shytskyi <111076900+shytskyi@users.noreply.github.com>
2023-01-22 12:25:25 +0100

    L16  homework

---
+++ Ivan_Shytskyi/Lesson_16/Lesson_16.Homework/Candidate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_16.Homework
+{
+    internal class Candidate
+    {
+    }
+}
---
+++ Ivan_Shytskyi/Lesson_16/Lesson_16.Homework/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson_16.Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
