43ee5d81d80472890141b80ad596b9dea7b863c4
Roman Bychkov <roman.bychkov.1921@gmail.com>
2023-01-27 17:27:42 +0200

    Moved GetGenerics to a separate method

--- Roman Bychkov/Lesson20/Lesson20.HomeWork/MyReflection.cs
+++ Roman Bychkov/Lesson20/Lesson20.HomeWork/MyReflection.cs
@@ -39,19 +39,7 @@ public static class MyReflection
 
         if (obj.FieldType.IsGenericType)
         {
-            for (int i = 0; i < obj.FieldType.GenericTypeArguments.Length; i++)
-            {
-
-                if (i == 0)
-                    generic.Append("<");
-                generic.Append(obj.FieldType.GenericTypeArguments[i].Name);
-                if (i < obj.FieldType.GenericTypeArguments.Length - 1)
-                    generic.Append(",");
-                if (i + 1 == obj.FieldType.GenericTypeArguments.Length)
-                    generic.Append(">");
-
-            }
-
+            generic.Append(GetGenerics(obj.FieldType.GenericTypeArguments));
         }
         else
             generic.Append("");
@@ -75,22 +63,11 @@ public static class MyReflection
                 param.Append("ref ");
             if (parameters[i].ParameterType.IsGenericType)
             {
-                for (int j = 0; j < parameters[i].ParameterType.GenericTypeArguments.Length; j++)
-                {
-
-                    if (j == 0)
-                        generic.Append("<");
-                    generic.Append(parameters[i].ParameterType.GenericTypeArguments[j].Name);
-                    if (j < parameters[i].ParameterType.GenericTypeArguments.Length - 1)
-                        generic.Append(",");
-                    if (j + 1 == parameters[i].ParameterType.GenericTypeArguments.Length)
-                        generic.Append(">");
-
-                }
-
+                generic.Append(GetGenerics(parameters[i].ParameterType.GenericTypeArguments));
             }
             param.Append(parameters[i].ParameterType.Name + generic + " " + parameters[i].Name);
             generic.Clear();
+
             if (parameters[i].IsOptional)
             {
                 if (parameters[i].DefaultValue == null)
@@ -108,7 +85,13 @@ public static class MyReflection
         if (obj is ConstructorInfo ctor)
             return $"{modificator} {ctor.DeclaringType} {param}";
         if (obj is MethodInfo method)
+        {
+            if (method.ReturnType.IsGenericType)
+            {
+                generic.Append(GetGenerics(method.ReturnType.GenericTypeArguments));
+            }
             return $"{modificator} {method.ReturnType.Name}{generic} {method.Name} {param}";
+        }
         else return "";
     }
     public static string GetEventInfo(EventInfo obj)
@@ -137,5 +120,21 @@ public static class MyReflection
             modificator.Append("protected internal");
         if (obj.IsStatic) modificator.Append(" static");
         return modificator.ToString();
+    }
+    public static string GetGenerics(Type[] type)
+    {
+        StringBuilder generic = new StringBuilder();
+        for (int j = 0; j < type.Length; j++)
+        {
+            if (j == 0)
+                generic.Append("<");
+            generic.Append(type[j].Name);
+            if (j < type.Length - 1)
+                generic.Append(",");
+            if (j + 1 == type.Length)
+                generic.Append(">");
+
+        }
+        return generic.ToString();
     }
 }
