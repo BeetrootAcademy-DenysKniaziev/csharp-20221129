8a6c081b42560be4f05dfe7f905990b7bece2cab
Roman Bychkov <roman.bychkov.1921@gmail.com>
2023-01-27 18:43:04 +0200

    Initial commit

---
+++ Roman Bychkov/Lesson21/CalendarApp/CalendarApp.Console/Presenters/MainMenuPresenter.cs
@@ -0,0 +1,45 @@
+﻿using CalendarApp.BLL.Services.Interfaces;
+using CalendarApp.Console.Presenters.Interfaces;
+using CalendarApp.Console.Presenters.Meetings;
+using CalendarApp.Contracts.Models;
+using static System.Console;
+
+namespace CalendarApp.Console.Presenters
+{
+    internal class MainMenuPresenter : IPresenter
+    {
+        private readonly IService<Meeting> _meetingsService;
+
+        public MainMenuPresenter(IService<Meeting> meetingsService)
+        {
+            _meetingsService = meetingsService;
+        }
+
+        public IPresenter Action()
+        {
+            var key = ReadKey();
+
+            switch (key.Key)
+            {
+                case ConsoleKey.D1:
+                    return new GetAllMeetingsPresenter(_meetingsService);
+                case ConsoleKey.D2:
+                    return new AddMeetingPresenter(_meetingsService);
+                case ConsoleKey.D0:
+                    return null;
+                default:
+                    return this;
+            }
+        }
+
+        public void Show()
+        {
+            Clear();
+
+            WriteLine("Select Action:");
+            WriteLine("1 - Get All Meetings");
+            WriteLine("2 - Add Meeting");
+            WriteLine("0 - Exit");
+        }
+    }
+}
---
+++ Roman Bychkov/Lesson21/CalendarApp/CalendarApp.Console/Presenters/Meetings/GetAllMeetingsPresenter.cs
@@ -0,0 +1,36 @@
+﻿using CalendarApp.BLL.Services.Interfaces;
+using CalendarApp.Console.Presenters.Interfaces;
+using CalendarApp.Contracts.Models;
+
+using static System.Console;
+
+namespace CalendarApp.Console.Presenters.Meetings
+{
+    internal class GetAllMeetingsPresenter : IPresenter
+    {
+        private readonly IService<Meeting> _service;
+
+        public GetAllMeetingsPresenter(IService<Meeting> service)
+        {
+            _service = service;
+        }
+
+        public IPresenter Action()
+        {
+            WriteLine("Press any key to continue...");
+            ReadKey();
+            return new MainMenuPresenter(_service);
+        }
+
+        public void Show()
+        {
+            Clear();
+
+            WriteLine("{0,-25}{1,-25}{2,-25}{3,-25}", "Name", "Start Time", "End Time", "Room Name");
+            foreach (var meeting in _service.GetAll())
+            {
+                WriteLine("{0,-25}{1,-25}{2,-25}{3,-25}", meeting.Name, meeting.StartTime, meeting.EndTime, meeting.RoomName);
+            }
+        }
+    }
+}
