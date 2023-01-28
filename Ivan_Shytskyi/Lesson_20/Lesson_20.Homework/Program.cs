f14ec298eb2a732c2b27e2a680efe183fcea4d6e
shytskyi <111076900+shytskyi@users.noreply.github.com>
2023-01-28 22:35:43 +0100

    l20

--- Ivan_Shytskyi/Lesson_20/Lesson_20.Homework/Program.cs
+++ Ivan_Shytskyi/Lesson_20/Lesson_20.Homework/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Lesson_20.Homework
+﻿using System.Reflection;
+
+namespace Lesson_20.Homework
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Assembly a = Assembly.LoadFile(@"D:\Academia_BeetRoot\csharp-20221129\Ivan Shytskyi\Ivan_Shytskyi\Lesson_13\Lesson_13.Homework\bin\Debug\net6.0\Lesson_13.Homework.dll");
+            Type[] types = a.GetTypes();
+
+            foreach (Type t in types)
+            {
+                var fields = t.GetFields(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+                foreach (var field in fields)
+                {
+                    Console.WriteLine($"{field.Name}");
+                }
+                Console.WriteLine();
+            }
+            foreach (Type t in types)
+            {
+                var methods = t.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+                foreach (var method in methods)
+                {
+                    Console.WriteLine($"{method.Name}");
+                }
+                Console.WriteLine();
+            }
+            foreach (Type t in types)
+            {
+                var properties = t.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+                foreach (var propertie in properties)
+                {
+                    Console.WriteLine($"{propertie.Name}");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
