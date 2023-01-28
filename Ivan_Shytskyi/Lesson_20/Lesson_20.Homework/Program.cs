17358a32e655dd50df622bd4c5c435271b895129
shytskyi <111076900+shytskyi@users.noreply.github.com>
2023-01-28 22:34:31 +0100

    Initial Commit

---
+++ Ivan_Shytskyi/Lesson_20/Lesson_20.Homework/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson_20.Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
