16b05c023822f12ef3f59e79ea956a053b36fc57
Egor <egorbf2003@gmail.com>
2023-01-31 20:31:32 +0200

    Egor Bogachik. Home work 16

---
+++ Egor Bogachick/Lesson16/Lesson16.HomeWork/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Lesson16.HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\nVote Application");
+                Console.WriteLine("Select Action:");
+                Console.WriteLine("1 - Create voting theme");
+                Console.WriteLine("2 - Vote");
+                Console.WriteLine("3 - All voters");
+                Console.WriteLine("4 - Selected voters");
+                Console.WriteLine("0 - Exit");
+
+                var action = Console.ReadKey();
+                Console.WriteLine();
+
+                switch (action.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine("Phone Book:");
+                        
+                        break;
+                    case ConsoleKey.D2:
+                        Console.WriteLine("Search:");
+                        
+                        break;
+                    case ConsoleKey.D3:
+                        Console.WriteLine("Add:");
+                        
+                        break;
+                    case ConsoleKey.D4:
+                        Console.WriteLine("Update:");
+                        
+                        break;
+                    case ConsoleKey.D5:
+                        Console.WriteLine("Remove:");
+                       
+                        break;
+                    case ConsoleKey.D0:
+                        Console.WriteLine("Exit");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect Input!");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Egor Bogachick/Lesson16/Lesson16.HomeWork/Vote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson16.HomeWork
+{
+    public class Vote
+    {
+        List<string> Thems { get; set; }
+        Dictionary<string, string> Voters { get; set; }
+
+
+    }
+}
