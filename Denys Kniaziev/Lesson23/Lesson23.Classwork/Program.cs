6e0a105490f58514af06276e296df940abb5784e
SteranOo <denisknyazev96@gmail.com>
2023-01-31 21:16:41 +0100

    Lesson 23. Classwork

---
+++ Denys Kniaziev/Lesson23/Lesson23.Classwork/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Lesson23.Classwork
+{
+    internal class Program
+    {
+        static async void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            var tasks = new List<Task>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var x = i;
+                tasks.Add(Print(x.ToString(), cts.Token));
+            }
+
+            Task.Run(async () =>
+            {
+                await Task.Delay(1800);
+                cts.Cancel();
+            });
+
+            Task.WaitAll(tasks.ToArray(), 10000);
+
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+
+            await GetNumber();
+        }
+
+        static async Task Print(string text, CancellationToken token)
+        {
+            Console.WriteLine("TID: " + Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+            if (token.IsCancellationRequested)
+                return;
+
+            Console.WriteLine(text);
+
+            if (token.IsCancellationRequested)
+                return;
+
+            await Task.Delay(TimeSpan.FromSeconds(2));
+        }
+
+        static async Task<int> GetNumberAsync()
+        {
+            await Task.Delay(1000);
+            return 10;
+        }
+
+        static ValueTask<int> GetNumber()
+        {
+            return ValueTask.FromResult(10);
+        }
+    }
+}
---
+++ Denys Kniaziev/Lesson23/Lesson23.Homework/Program.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Lesson23.Homework
+{
+    internal class Program
+    {
+        class RequestBody
+        {
+            [JsonProperty("prompt")]
+            public string Request { get; set; }
+
+            [JsonProperty("n")]
+            public int ImagesNumber { get; set; }
+
+            [JsonProperty("size")]
+            public string ImagesSize { get; set; }
+
+            [JsonProperty("response_format")]
+            public string Format { get; set; }
+        }
+
+        class ResponseBody
+        {
+            public class DataItems
+            {
+                [JsonProperty("url")]
+                public string Url { get; set; }
+            }
+
+            [JsonProperty("created")]
+            public long Created { get; set; }
+
+            [JsonProperty("data")]
+            public List<DataItems> Data { get; set; }
+        }        
+
+        static async Task Main(string[] args)
+        {
+            using var client = new HttpClient
+            {
+                BaseAddress = new Uri("https://api.openai.com"),
+                Timeout = TimeSpan.FromSeconds(100),
+                
+            };
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "[PRIVATE_KEY]");
+
+            Console.WriteLine("Image Description:");
+            var request = Console.ReadLine();
+
+            var body = new RequestBody
+            {
+                Request = request,
+                ImagesNumber = 1,
+                ImagesSize = "1024x1024",
+                Format = "url"
+            };
+
+            var response = await client.PostAsync(
+                "v1/images/generations",
+                new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json"));           
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var responseBody = JsonConvert.DeserializeObject<ResponseBody>(responseContent);
+
+            using var imageClient = new HttpClient();
+
+            for (int i = 0; i < responseBody.Data.Count; i++)
+            {
+                var data = responseBody.Data[i];
+                var imageResponse = await imageClient.GetAsync(data.Url);
+                var imageBytes = await imageResponse.Content.ReadAsByteArrayAsync();
+                File.WriteAllBytes($"img{i}.jpg", imageBytes);
+                Process.Start("explorer.exe", $"img{i}.jpg");
+            }
+        }
+    }
+}
