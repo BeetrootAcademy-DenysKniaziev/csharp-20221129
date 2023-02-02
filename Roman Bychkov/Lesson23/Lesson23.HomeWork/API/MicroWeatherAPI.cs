c0f7f7882ee5610b342ce6b2b157ae740a5d4faf
Roman Bychkov <roman.bychkov.1921@gmail.com>
2023-02-02 17:07:15 +0200

    Initial commit

---
+++ Roman Bychkov/Lesson23/Lesson23.HomeWork/API/MicroWeatherAPI.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Lesson23.HomeWork
+{
+    internal class MicroWeatherAPI
+    {
+        private string _token;
+       
+        public MicroWeatherAPI(string token = "ZTljOWQzOWYtNGJkYy00NjRlLThlM2MtYzRjZDQ2YTI3MTcx")
+        {
+            _token = token;
+        }
+        public async Task<string> CreateRequestAsync(string city)
+        {
+            using var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://api.m3o.com");
+            httpClient.Timeout = TimeSpan.FromSeconds(10);
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+
+            var _body = new RequestBody
+            {
+                Location = city,
+            };
+            var response = await httpClient.PostAsync(
+               "v1/weather/Now",
+               new StringContent(JsonConvert.SerializeObject(_body), Encoding.UTF8, "application/json"));
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            string result = JsonConvert.DeserializeObject<ResponseBody>(responseContent).ToString();
+            Console.WriteLine(result);
+            return result;
+
+        }
+        public async Task<LatitudeLongitude> GetLatitudeLongitude(string city)
+        {
+            using var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://api.m3o.com");
+            httpClient.Timeout = TimeSpan.FromSeconds(10);
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+
+            var _body = new RequestBody
+            {
+                Location = city,
+            };
+            var response = await httpClient.PostAsync(
+               "v1/weather/Now",
+               new StringContent(JsonConvert.SerializeObject(_body), Encoding.UTF8, "application/json"));
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<LatitudeLongitude>(responseContent);
+
+        }
+
+
+        class RequestBody
+        {
+            [JsonProperty("location")]
+            public string Location { get; set; }
+        }
+        class ResponseBody
+        {
+            [JsonProperty("region")]
+            public string Region { get; set; }
+
+            [JsonProperty("temp_c")]
+            public double TempC { get; set; }
+
+            [JsonProperty("feels_like_c")]
+            public double FeelsLikeC { get; set; }
+
+            [JsonProperty("cloud")]
+            public double Cloud { get; set; }
+
+            [JsonProperty("condition")]
+            public string Condition { get; set; }
+
+            public override string ToString()
+            {
+                return $"MicroWeather || Region : {Region}, Temp C: {TempC}, Feels Like: {FeelsLikeC}, Cloud: {Cloud}, Condition: {Condition}";
+            }
+        }
+        public class LatitudeLongitude
+        {
+            [JsonProperty("latitude")]
+            public double Latitude { get; set; }
+
+            [JsonProperty("longitude")]
+            public double Longitude { get; set; }
+            public override string ToString()
+            {
+                return $"Latitude : {Latitude}, Longitude : {Longitude}";
+            }
+        }
+
+    }
+}
---
+++ Roman Bychkov/Lesson23/Lesson23.HomeWork/API/OpenMeteoAPI.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace Lesson23.HomeWork.API
+{
+    internal class OpenMeteoAPI
+    {
+
+        public async Task<string> CreateRequestAsync(double latitude, double longitude)
+        {
+            using var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://api.open-meteo.com");
+            httpClient.Timeout = TimeSpan.FromSeconds(10);
+            httpClient.DefaultRequestHeaders.Authorization = null;
+
+            var response = await httpClient.GetAsync(
+               $"v1/forecast?latitude="+latitude.ToString().Replace(',', '.')+"&longitude=" + longitude.ToString().Replace(',', '.') + "&current_weather=true");
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            string result = JsonConvert.DeserializeObject<ResponseBody>(responseContent).ToString();
+            Console.WriteLine(result);
+            return result;
+        }
+
+        class ResponseBody
+        {
+
+            [JsonProperty("current_weather")]
+            public ResponseBodyInnere Info { get; set; }
+
+            public override string ToString()
+            {
+                return $"OpenMeteo || Temp C: {Info.TempC}, WindSpeed: {Info.WindSpeed}";
+            }
+        }
+        class ResponseBodyInnere
+        {
+            [JsonProperty("temperature")]
+            public double TempC { get; set; }
+
+            [JsonProperty("windspeed")]
+            public double WindSpeed { get; set; }
+        }
+    }
+}
