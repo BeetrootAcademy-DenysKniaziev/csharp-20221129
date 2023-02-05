62fca72634e7cb02f167cd164369234573048e88
IMBomb <Bonislavskiy@gmail.com>
2023-02-05 06:59:40 +0100

    Homework23 Added

---
+++ Lesson23/Homework23/ImageToText.cs
@@ -0,0 +1,90 @@
+﻿using Lesson23;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+using System.IO;
+using System;
+using System.Windows.Forms;
+
+namespace Homework23
+{
+
+    public class Result
+    {
+        public List<Tag> tags { get; set; }
+    }
+
+    public class Root
+    {
+        public Result result { get; set; }
+        public Status status { get; set; }
+    }
+
+    public class Status
+    {
+        public string text { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Tag
+    {
+        public double confidence { get; set; }
+        public Tag2 tag { get; set; }
+    }
+
+    public class Tag2
+    {
+        public string en { get; set; }
+    }
+    internal class ImageToText
+    {
+        public static string ConvertImageToText(string imageUrl)
+        {
+            // fileName = "https://imagga.com/static/images/tagging/wind-farm-538576_640.jpg";
+
+            string apiKey = "acc_3407d769a837da1";
+            string apiSecret = "eca9b760cf4df75010bfcee655cf1b67";
+            //string imageUrl = "https://imagga.com/static/images/tagging/wind-farm-538576_640.jpg";
+
+            string basicAuthValue = System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(String.Format("{0}:{1}", apiKey, apiSecret)));
+
+            var client = new RestClient("https://api.imagga.com/v2/tags");
+
+            var request = new RestRequest();
+            request.AddParameter("image_url", imageUrl);
+            request.AddHeader("Authorization", String.Format("Basic {0}", basicAuthValue));
+
+            var response = client.Execute(request);
+
+            var json = Newtonsoft.Json.JsonConvert.DeserializeObject(response.Content);
+            var goodLookingResponce = JsonConvert.SerializeObject(json, Formatting.Indented);
+
+            System.IO.File.WriteAllText("prediction.json", goodLookingResponce);
+            var root = JsonConvert.DeserializeObject<Root>(goodLookingResponce);
+
+            var InImage = root.result.tags.Select(t => new { tag = t.tag.en, confidence = t.confidence }).Where(c => c.confidence > 35);
+            //Console.WriteLine(goodLookingResponce);
+            var autoRequest = "";
+            Console.WriteLine(imageUrl + "\nOne AI Saw on the picture:");
+            foreach (var i in InImage)
+            {
+                var rnd = new Random().Next(0,3);
+                if (rnd == 1)
+                {
+                    autoRequest += i.tag + ", ";
+                    Console.WriteLine("- " + i.tag + " " + (int)i.confidence + "%");
+                }
+            }
+            if (autoRequest == "") autoRequest = "empty";
+            Console.WriteLine("Another AI Drawing Based On This...\n");
+            //Console.ReadLine();
+            return autoRequest;
+        }
+    }
+}
---
+++ Lesson23/Homework23/TextToImage.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Lesson23.Homework
+{
+    internal class TextToImage
+    {
+
+        class RequestBody
+        {
+            [JsonProperty("prompt")]
+            public string Request { get; set; }
+
+            [JsonProperty("n")]
+            public int ImagesNumber { get; set; }
+
+            [JsonProperty("size")]
+            public string ImagesSize { get; set; }
+
+            [JsonProperty("response_format")]
+            public string Format { get; set; }
+        }
+
+        class ResponseBody
+        {
+            public class DataItems
+            {
+                [JsonProperty("url")]
+                public string Url { get; set; }
+            }
+
+            [JsonProperty("created")]
+            public long Created { get; set; }
+
+            [JsonProperty("data")]
+            public List<DataItems> Data { get; set; }
+        }
+
+        public static async Task ConvertTextToImage(string request)
+        {
+            using var client = new HttpClient
+            {
+                BaseAddress = new Uri("https://api.openai.com"),
+                Timeout = TimeSpan.FromSeconds(100),
+
+            };
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "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");
+
+            //Console.WriteLine("Image Description:");
+            //var request = Console.ReadLine();
+
+            var body = new RequestBody
+            {
+                Request = request,
+                ImagesNumber = 1,
+                ImagesSize = "1024x1024",
+                Format = "url"
+            };
+
+            var response = await client.PostAsync(
+                "v1/images/generations",
+                new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json"));
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var responseBody = JsonConvert.DeserializeObject<ResponseBody>(responseContent);
+
+            using var imageClient = new HttpClient();
+
+            for (int i = 0; i < responseBody.Data.Count; i++)
+            {
+                var data = responseBody.Data[i];
+                var imageResponse = await imageClient.GetAsync(data.Url);
+                var imageBytes = await imageResponse.Content.ReadAsByteArrayAsync();
+                File.WriteAllBytes($"img{i}.jpg", imageBytes);
+                Process.Start("explorer.exe", $"img{i}.jpg");
+            }
+        }
+    }
+}
+
+
