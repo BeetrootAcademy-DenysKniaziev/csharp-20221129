a6cfdbf72fe73fd0918959bee56297be985e328a
RomanNoname <105872125+RomanNoname@users.noreply.github.com>
2023-02-05 22:35:30 +0200

    RomanBychkov homework20 (#169)
    * Initial commit
    
    * Added project
    
    * Roman Bychkov Homework20
    
    * Moved GetGenerics to a separate method

---
+++ Roman Bychkov/Lesson20/Lesson20.HomeWork/MyReflection.cs
@@ -0,0 +1,140 @@
+﻿using System.Reflection;
+using System.Text;
+
+public static class MyReflection
+{
+    public static string GetPropertyInfo(PropertyInfo obj)
+    {
+        StringBuilder result = new StringBuilder($"{obj.PropertyType.Name} {obj.Name} {{");
+
+
+        if (obj.CanRead)
+        {
+            result.Append(GetModificator(obj.GetMethod) + " get;");
+        }
+
+        if (obj.CanWrite) result.Append(GetModificator(obj.SetMethod) + " set;");
+        result.Append("}");
+        return result.ToString();
+    }
+
+    public static string GetFieldInfo(FieldInfo obj)
+    {
+
+        StringBuilder modificator = new StringBuilder();
+        StringBuilder generic = new StringBuilder();
+        if (obj.IsPublic)
+            modificator.Append("public");
+        else if (obj.IsPrivate)
+            modificator.Append("private");
+        else if (obj.IsAssembly)
+            modificator.Append("internal");
+        else if (obj.IsFamily)
+            modificator.Append("protected");
+        else if (obj.IsFamilyAndAssembly)
+            modificator.Append("private protected");
+        else if (obj.IsFamilyOrAssembly)
+            modificator.Append("protected internal");
+        if (obj.IsStatic) modificator.Append(" static");
+
+        if (obj.FieldType.IsGenericType)
+        {
+            generic.Append(GetGenerics(obj.FieldType.GenericTypeArguments));
+        }
+        else
+            generic.Append("");
+
+        return $"{modificator} {obj.FieldType.Name} {generic.ToString()} {obj.Name}";
+
+    }
+    public static string GetMethodBaseInfo(MethodBase obj)
+    {
+        string modificator = GetModificator(obj);
+        StringBuilder param = new StringBuilder("(");
+        StringBuilder generic = new StringBuilder();
+        ParameterInfo[] parameters = obj.GetParameters();
+        for (int i = 0; i < parameters.Length; i++)
+        {
+            if (parameters[i].IsIn)
+                param.Append("in ");
+            if (parameters[i].IsOut)
+                param.Append("out ");
+            if (parameters[i].ParameterType.IsByRef)
+                param.Append("ref ");
+            if (parameters[i].ParameterType.IsGenericType)
+            {
+                generic.Append(GetGenerics(parameters[i].ParameterType.GenericTypeArguments));
+            }
+            param.Append(parameters[i].ParameterType.Name + generic + " " + parameters[i].Name);
+            generic.Clear();
+
+            if (parameters[i].IsOptional)
+            {
+                if (parameters[i].DefaultValue == null)
+                    param.Append(" = null");
+                else
+                    param.Append(" = " + parameters[i].DefaultValue);
+
+            }
+
+            if (i < parameters.Length - 1)
+                param.Append(", ");
+
+        }
+        param.Append(")");
+        if (obj is ConstructorInfo ctor)
+            return $"{modificator} {ctor.DeclaringType} {param}";
+        if (obj is MethodInfo method)
+        {
+            if (method.ReturnType.IsGenericType)
+            {
+                generic.Append(GetGenerics(method.ReturnType.GenericTypeArguments));
+            }
+            return $"{modificator} {method.ReturnType.Name}{generic} {method.Name} {param}";
+        }
+        else return "";
+    }
+    public static string GetEventInfo(EventInfo obj)
+    {
+        StringBuilder modificator = new StringBuilder();
+        if (obj.IsMulticast)
+            modificator.Append("multicast ");
+        if (obj.IsCollectible)
+            modificator.Append("collectible ");
+        return $"{modificator} {obj.EventHandlerType.Name} {obj.Name}";
+    }
+    public static string GetModificator(MethodBase obj)
+    {
+        StringBuilder modificator = new StringBuilder();
+        if (obj.IsPublic)
+            modificator.Append("public");
+        else if (obj.IsPrivate)
+            modificator.Append("private");
+        else if (obj.IsAssembly)
+            modificator.Append("internal");
+        else if (obj.IsFamily)
+            modificator.Append("protected");
+        else if (obj.IsFamilyAndAssembly)
+            modificator.Append("private protected");
+        else if (obj.IsFamilyOrAssembly)
+            modificator.Append("protected internal");
+        if (obj.IsStatic) modificator.Append(" static");
+        return modificator.ToString();
+    }
+    public static string GetGenerics(Type[] type)
+    {
+        StringBuilder generic = new StringBuilder();
+        for (int j = 0; j < type.Length; j++)
+        {
+            if (j == 0)
+                generic.Append("<");
+            generic.Append(type[j].Name);
+            if (j < type.Length - 1)
+                generic.Append(",");
+            if (j + 1 == type.Length)
+                generic.Append(">");
+
+        }
+        return generic.ToString();
+    }
+}
---
+++ Roman Bychkov/Lesson20/Lesson20.HomeWork/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+
+public class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Input path:");
+        string path = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(path))
+            path = @"C:\Users\roman\source\repos\csharp-20221129\Roman Bychkov\Lesson20\Lesson17.HomeWork\bin\Debug\net6.0\Lesson17.HomeWork.dll";
+        Assembly asm = Assembly.LoadFrom(path);
+        Type[] types = asm.GetTypes();
+        foreach (Type t in types)
+        {
+            Console.WriteLine(t.Name);
+            Console.WriteLine("\tMethods:");
+            foreach (MethodInfo mi in t.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static))
+            {
+                Console.WriteLine("\t\t" + MyReflection.GetMethodBaseInfo(mi));
+            }
+            Console.WriteLine("\tConstructor:");
+            foreach (ConstructorInfo mi in t.GetConstructors(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static))
+            {
+                Console.WriteLine("\t\t" + MyReflection.GetMethodBaseInfo(mi));
+            }
+            Console.WriteLine("\tFields:");
+            foreach (FieldInfo mi in t.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static))
+            {
+                Console.WriteLine("\t\t" + MyReflection.GetFieldInfo(mi));
+            }
+            Console.WriteLine("\tProperties:");
+            foreach (PropertyInfo mi in t.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static))
+            {
+                Console.WriteLine("\t\t" + MyReflection.GetPropertyInfo(mi));
+            }
+            Console.WriteLine("\tEvents:");
+            foreach (EventInfo mi in t.GetEvents(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static))
+            {
+                Console.WriteLine("\t\t" + MyReflection.GetEventInfo(mi));
+            }
+          
+
+        }
+
+    }
+
+}
