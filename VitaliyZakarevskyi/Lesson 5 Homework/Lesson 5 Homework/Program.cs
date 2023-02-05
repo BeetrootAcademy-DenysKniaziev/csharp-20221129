fa63feea101a9591b07fceff9736d53579342214
vitovskyii <119495604+vitovskyii@users.noreply.github.com>
2023-02-05 19:56:59 +0200

    Vitaliyzakarevskiy Homework 5 (#119)
    * Lesson 5 Homework
    
    * Lesson 5 Homework

---
+++ VitaliyZakarevskyi/Lesson 5 Homework/Lesson 5 Homework/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Collections.Generic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        
+        Console.WriteLine(GCD(200, 100));
+        Console.WriteLine(PrimeNumber(154));
+
+    }
+
+    static int GCD(int value1, int value2)
+    {
+        int temp = 0;
+        for (int i = Math.Min(value1, value2); i > 0; i--)
+        {
+            if (value1 % i == 0 && value2 % i == 0)
+            {
+                temp = i;
+                break;
+                
+            } 
+        }
+        return temp;
+
+    }
+        static int PrimeNumber(int value1)
+    {
+        int sum = 0;
+            for (int i = 2; i <= value1; i++)
+            {
+                if (value1 % i == 0)
+                {
+                    sum += i;
+                }
+                
+            }
+        return sum;
+    }    
+}
