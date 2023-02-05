745a3a9546f85e44ec0c768092caee1529fa0728
shytskyi <111076900+shytskyi@users.noreply.github.com>
2023-02-05 14:47:29 +0100

    add rooms.json

--- Ivan_Shytskyi/Lesson_22/Calendar.DAL/Repositorys/MeetingsRepository.cs
+++ Ivan_Shytskyi/Lesson_22/Calendar.DAL/Repositorys/MeetingsRepository.cs
@@ -1,17 +1,34 @@
-﻿using System;
-using Calendar.DAL.Repositorys.Interface;
+﻿using Calendar.DAL.Repositorys.Interface;
 using Calendar.Contracts.Models;
 using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Calendar.DAL.Repositorys
 {
     internal class MeetingsRepository : IRepository<Meeting>
     {
-        private readonly List<Meeting> _meetings = new List<Meeting>
+        private const string FileName = "Meetings.json";
+        //private readonly List<Meeting> _meetings = new List<Meeting>
+        //{
+        //    new Meeting("Test Meeting", DateTime.UtcNow, DateTime.UtcNow.AddHours(8), "Green")
+        //};
+        public void Add(Meeting meeting) //=> _meetings.Add(meeting);
         {
-            new Meeting("Test Meeting", DateTime.UtcNow, DateTime.UtcNow.AddHours(8), "Green")
-        };
-        public void Add(Meeting meeting) => _meetings.Add(meeting);
-        public IEnumerable<Meeting> GetAll() => _meetings;
+            var meetings = GetAll();
+            meetings = meetings.Append(meeting);
+            var json = JsonConvert.SerializeObject(meetings, Formatting.Indented);
+            File.WriteAllText(FileName, json);
+        }
+        public IEnumerable<Meeting> GetAll() //=> _meetings;
+        {
+            if (!File.Exists(FileName))
+                return Enumerable.Empty<Meeting>();
+            var json = File.ReadAllText(FileName);
+            //var meeting = JsonConvert.DeserializeObject<IEnumerable<Meeting>>(json)  == null ? Enumerable.Empty<Meeting>() : JsonConvert.DeserializeObject<IEnumerable<Meeting>>(json);
+            var meetings = JsonConvert.DeserializeObject<IEnumerable<Meeting>>(json) ?? Enumerable.Empty<Meeting>();
+            return meetings;
+        }
     }
 }
--- Ivan_Shytskyi/Lesson_22/Calendar.DAL/Repositorys/RoomsRepository.cs
+++ Ivan_Shytskyi/Lesson_22/Calendar.DAL/Repositorys/RoomsRepository.cs
@@ -1,13 +1,29 @@
 ﻿using Calendar.Contracts.Models;
 using Calendar.DAL.Repositorys.Interface;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
 
 namespace Calendar.DAL.Repositorys
 {
     internal class RoomsRepository : IRepository<Room>
     {
-        protected readonly List<Room> _rooms = new List<Room>();
-        public void Add(Room room) => _rooms.Add(room);
-        public IEnumerable<Room> GetAll() => _rooms;
+        private const string FileName = "Rooms.json";
+        public void Add(Room room)
+        {
+            var rooms = GetAll();
+            rooms = rooms.Append(room);
+            var json = JsonConvert.SerializeObject(rooms, Formatting.Indented);
+            File.WriteAllText(FileName, json);
+        }
+        public IEnumerable<Room> GetAll()
+        {
+            if (!File.Exists(FileName))
+                return Enumerable.Empty<Room>();
+            var json = File.ReadAllText(FileName);
+            var rooms = JsonConvert.DeserializeObject<IEnumerable<Room>>(json) ?? Enumerable.Empty<Room>();
+            return rooms;
+        }
     }
 }
