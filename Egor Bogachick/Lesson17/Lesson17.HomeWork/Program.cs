de05bc50f3da62eb1d2993ff8dcf18f8a7ab25f9
kovasnik <egorbf2003@gmail.com>
2023-02-09 00:04:04 +0200

    Egor Bogachik. Home work 17

---
+++ Egor Bogachick/Lesson17/Lesson17.HomeWork/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson17.HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
