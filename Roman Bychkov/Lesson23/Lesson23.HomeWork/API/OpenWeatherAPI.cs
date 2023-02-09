8b86dff196e7a2df49eedb2f30d07e2ce53a4268
RomanNoname <105872125+RomanNoname@users.noreply.github.com>
2023-02-09 13:57:06 +0200

    Roman Bychkov homework23 (#181)
    * Initial commit
    
    * Roman Bychkov homework23
    
    * Updated
    
    * Fixed bug

---
+++ Roman Bychkov/Lesson23/Lesson23.HomeWork/API/OpenWeatherAPI.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+
+namespace Lesson23.HomeWork.API
+{
+    internal class OpenWeatherAPI
+    {
+        private string _token;
+
+        public OpenWeatherAPI(string token)
+        {
+            _token = token;
+        }
+        public async Task<string> CreateRequestAsync(double latitude, double longitude)
+        {
+            using var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://api.openweathermap.org");
+            httpClient.Timeout = TimeSpan.FromSeconds(10);
+            
+            var response = await httpClient.GetAsync(
+               $"data/2.5/weather?lat=" + latitude.ToString().Replace(',', '.') + "&lon=" + longitude.ToString().Replace(',', '.') + "&appid=" + _token);
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            string result = JsonConvert.DeserializeObject<ResponseBody>(responseContent).ToString();
+            Console.WriteLine(result);
+            return result;
+        }
+        class ResponseBody
+        {
+
+            [JsonProperty("main")]
+            public ResponseBodyInner Info { get; set; }
+
+            public override string ToString()
+            {
+                return $"OpenWeather || Temp C: {Info.TempK - 273}, Feels like: {Info.FeelsLike - 273}";
+            }
+        }
+        class ResponseBodyInner
+        {
+            [JsonProperty("temp")]
+            public double TempK { get; set; }
+
+            [JsonProperty("feels_like")]
+            public double FeelsLike { get; set; }
+        }
+
+
+    }
+}
---
+++ Roman Bychkov/Lesson23/Lesson23.HomeWork/Program.cs
@@ -0,0 +1,52 @@
+﻿using Lesson23.HomeWork;
+using Lesson23.HomeWork.API;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+using static Lesson23.HomeWork.MicroWeatherAPI;
+
+class Program
+{
+
+    static async Task Main()
+    {
+      
+        var microWeatherClient = new MicroWeatherAPI("token");
+        var openMeteoClient = new OpenMeteoAPI();
+        var OpenWeather = new OpenWeatherAPI("token");
+        LatitudeLongitude coordinateCity;
+        string cityName;
+
+        while (true)
+        {
+            Console.WriteLine("Enter city name:");
+            cityName = Console.ReadLine();
+            coordinateCity = await microWeatherClient.GetLatitudeLongitude(cityName);
+            Console.WriteLine();
+          
+            var task1 = microWeatherClient.CreateRequestAsync(cityName);
+            var task2 = openMeteoClient.CreateRequestAsync(coordinateCity.Latitude, coordinateCity.Longitude);
+            var task3 = OpenWeather.CreateRequestAsync(coordinateCity.Latitude, coordinateCity.Longitude);
+
+            var allTasks = Task.WhenAll(task1, task2, task3);
+
+            try
+            {
+                await allTasks;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+                Console.WriteLine($"IsFaulted: {allTasks.IsFaulted}");
+                if (allTasks.Exception is not null)
+                {
+                    foreach (var exception in allTasks.Exception.InnerExceptions)
+                    {
+                        Console.WriteLine($"InnerException: {exception.Message}");
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
