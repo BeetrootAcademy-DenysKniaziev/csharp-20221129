fe0837ac87290a9f28e0bfc2de82cc0dcd444f42
kovasnik <egorbf2003@gmail.com>
2023-02-16 03:05:03 +0200

    Egor Bogachik. Home work 19

--- Egor Bogachick/Lesson19/Lesson19.Homeork/Program.cs
+++ Egor Bogachick/Lesson19/Lesson19.Homeork/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.IO;
 using System;
@@ -18,23 +19,145 @@ namespace Lesson19.Homeork
                 new Customer("Ben", new DateTime(1983, 9, 23), new List<string> {"673555431"}),
                 new Customer("Sarah", new DateTime(1990, 2, 6), new List<string> {"671675380", "672135768", "672121655"})
             };
+            #region SELECT
+            #region Завдання 1: Вивести імена та вік клієнтів
+            ////var clients = from c in customers
+            ////              select new
+            ////              {
+            ////                  Name = c.Name,
+            ////                  Age = DateTime.Now.Year - c.Birthday.Year 
+            ////              };
 
-            //SELECT
-            //Завдання 1: Вивести імена та вік клієнтів
-            //Завдання 2: Вивести всі телефонні номери клієнтів з кодом +380
-            //Завдання 3: Вивести всі телефонні номери з інформацією про клієнта для кожного номеру
+            //var clients = customers.Select(c => new
+            //{
+            //    Name = c.Name,
+            //    Age = DateTime.Now.Year - c.Birthday.Year
+            //} );
 
-            //WHERE:
-            //Завдання 1: Вивести інформацією про клієнтів, хто народився у січні
-            //Завдання 2: Вивести інформацією про клієнтів, імена яких мають непарну довжину
-            //Завдання 3: Вивести інформацією про клієнтів, хто народилися у парному місяці року та хоча б один номер яких починається з "671"
+            ////clients.ToList().ForEach(Console.WriteLine);
+            //foreach (var client in clients)
+            //{
+            //    Console.WriteLine(client.Name + " " + client.Age);
+            //}
+            #endregion
+
+            #region Завдання 2: Вивести всі телефонні номери клієнтів з кодом +380
+            ////var numbers1 = from c in customers
+            ////               from p in c.PhoneNumbers
+            ////              select new
+            ////              {
+            ////                  Name = c.Name,
+            ////                  Number = $"+360{p}"
+            ////              };
+
+            //var numbers1 = customers.SelectMany(c => c.PhoneNumbers, (c, p) => new
+            //{
+            //    Name = c.Name,
+            //    Number = $"+360{p}"
+            //});
+            //foreach (var number in numbers1)
+            //{
+            //    Console.WriteLine(number.Name + " " + number.Number);
+            //}
+
+            #endregion
+
+            #region Завдання 3: Вивести всі телефонні номери з інформацією про клієнта для кожного номеру
+            ////var numbers2 = from c in customers
+            ////               from p in c.PhoneNumbers
+            ////               select new
+            ////               {
+            ////                   Customer = c,
+            ////                   Number = p
+            ////               };
+
+            //var numbers2 = customers.SelectMany(c => c.PhoneNumbers, (c, p) => new { Customer = c, Number = p});
+
+            //foreach (var number in numbers2)
+            //{
+            //    Console.WriteLine($"{number.Number}: {number.Customer.Name}, {number.Customer.Birthday.Day}/{number.Customer.Birthday.Month}/{number.Customer.Birthday.Year}");
+            //}
+
+            #endregion
+
+            #endregion
+
+            #region WHERE
+            #region Завдання 1: Вивести інформацією про клієнтів, хто народився у січні
+            ////var clients = from c in customers
+            ////              where c.Birthday.Month == 1
+            ////              select c;
+
+            //var clients = customers.Where(c => c.Birthday.Month == 1);
+
+            //foreach (var client in clients)
+            //{
+            //    Console.Write(client.Name + " " + client.Birthday + " ");
+            //    foreach (var phone in client.PhoneNumbers)
+            //    {
+            //        Console.Write(phone + " ");
+            //    }
+            //Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Завдання 2: Вивести інформацією про клієнтів, імена яких мають непарну довжину
+            ////var clients = from c in customers
+            ////             where c.Name.Length % 2 == 1
+            ////             select c;
+
+            //var clients = customers.Where(c => c.Name.Length % 2 == 1);
+
+            //foreach (var client in clients)
+            //{
+            //    Console.Write(client.Name + " " + client.Birthday + " ");
+            //    foreach (var phone in client.PhoneNumbers)
+            //    {
+            //        Console.Write(phone + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region Завдання 3: Вивести інформацією про клієнтів, хто народилися у парному місяці року та хоча б один номер яких починається з "671"
+            //var clients = from c in customers
+            //              where c.Birthday.Month % 2 == 0 && c.PhoneNumbers.Any(x => x.StartsWith("671"))
+            //              select c;
+
+            //var clients = customers.Where(c => c.Birthday.Month % 2 == 0 && c.PhoneNumbers.Any(x => x.StartsWith("671")));
+
+            //foreach (var client in clients)
+            //{
+            //    Console.Write(client.Name + " " + client.Birthday + " ");
+            //    foreach (var phone in client.PhoneNumbers)
+            //    {
+            //        Console.Write(phone + " ");
+            //    }
+            //Console.WriteLine();
+            //}
+
+            #endregion
+
+            #endregion
+
 
             var persons = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json"));
 
-            //find out who is located farthest north/south/west/east using latitude/longitude data
-            //find max and min distance between 2 persons
-            //find 2 persons whos ‘about’ have the most same words
-            //find persons with same friends(compare by friend’s name)            
+            #region third task
+            #region find out who is located farthest north/south/west/east using latitude/longitude data
+
+            #endregion
+            #region find max and min distance between 2 persons
+
+            #endregion
+            #region find 2 persons whos ‘about’ have the most same words
+
+            #endregion
+            #region find persons with same friends(compare by friend’s name)
+
+            #endregion
+            #endregion
         }
     }
 }
