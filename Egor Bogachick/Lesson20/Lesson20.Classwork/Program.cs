0c39a8086f29fe9f2a5cda5dd829565e42c64af6
kovasnik <egorbf2003@gmail.com>
2023-02-20 03:31:39 +0200

    Egor Bogachik. Home work 20

---
+++ Egor Bogachick/Lesson20/Lesson20.Classwork/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson20.Classwork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Egor Bogachick/Lesson20/Lesson20.HomeWork/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace Lesson20.HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly asm = Assembly.LoadFrom("C:\\Users\\bogac\\source\\repos\\csharp-20221129\\Egor Bogachick\\Lesson20\\Lesson20.FromLesson13\\bin\\Debug\\net6.0\\Lesson20.FromLesson13.dll");
+            Type[] type = asm.GetTypes();
+
+            Console.WriteLine(asm.FullName);
+        }
+    }
+}
