08117b11a80d854c564335268b47e2d191cb9a68
Egor Bogachik <79566284+kovasnik@users.noreply.github.com>
2023-02-23 15:32:51 +0200

    Egor Bogachik home work 20 (#213)
    * Egor Bogachik. Home work 14
    
    * Egor Bogachik. Home work 20
    
    * Egor Bogachik. Home work 20

---
+++ Egor Bogachick/Lesson20/Lesson20.FromLesson13/IShowable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson20.FromLesson13
+{
+    internal interface IShowable
+    {
+        public void Info();
+    }
+}
---
+++ Egor Bogachick/Lesson20/Lesson20.FromLesson13/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lesson20.FromLesson13
+{
+    public class Product : IShowable
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product() { }
+        public Product(string name, int price, int quqntity)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Quantity = quqntity;
+        }
+
+        public Product AddProduct()
+        {
+            int reg = 0;
+            Console.WriteLine("Enter product name:");
+            do
+            {
+                if (reg > 0)
+                {
+                    Console.WriteLine("\nIncorrect input, try again:");
+                }
+                Name = (Console.ReadLine()!);
+                reg++;
+            } while (!Regex.IsMatch(Name, @"[A-Za-z]"));
+
+            Console.WriteLine("Enter price:");
+            Price = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Enter quantity:");
+            Quantity = int.Parse(Console.ReadLine()!);
+            return new Product(Name, Price, Quantity);
+        }
+
+        private void ProductPrivateMethod()
+        {
+            Console.WriteLine("ProductPrivateMethod");
+        }
+        public void Info()
+        {
+            Console.Write("Name: " + Name + "  Price: " + Price + "  Quantity: " + Quantity);
+        }
+    }
+}
