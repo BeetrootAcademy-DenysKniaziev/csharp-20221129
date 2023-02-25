04ad842f02ca5aef3eda349297d61697bae06e1e
SteranOo <denisknyazev96@gmail.com>
2023-02-25 14:04:48 +0100

    Lesson 32.Classwork

---
+++ Denys Kniaziev/Lesson32/WebSite/js/main.js
@@ -0,0 +1,31 @@
+﻿let formAdd = document.getElementById('formAdd');
+let inpName = document.getElementById('inpName');
+let inpPrice = document.getElementById('inpPrice');
+let tblProductsBody = document.getElementById('tblProductsBody');
+
+let id = 0;
+
+formAdd.addEventListener('submit',
+    (event) => {
+        var name = inpName.value;
+        var price = inpPrice.value;
+
+        var tr = document.createElement('tr');
+
+        var td = document.createElement('td');
+        td.innerText = ++id;
+        tr.appendChild(td);
+
+        var td = document.createElement('td');
+        td.innerText = name;
+        tr.appendChild(td);
+
+        var td = document.createElement('td');
+        td.innerText = price;
+        tr.appendChild(td);
+
+        tblProductsBody.appendChild(tr);
+
+        confirm("?!!??");
+        event.preventDefault();
+    });
