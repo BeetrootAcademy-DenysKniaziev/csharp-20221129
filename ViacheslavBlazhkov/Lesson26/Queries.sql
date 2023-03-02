0af9e176c37b6da63564f9aeedfe25989ccb79e3
Viacheslav Blazhkov <79785720+ViacheslavBlazhkov@users.noreply.github.com>
2023-03-02 15:08:13 +0200

    Viacheslav blazhkov lesson 26 (#221)
    * Last commit
    
    * LC
    
    * commit

---
+++ ViacheslavBlazhkov/Lesson26/Queries.sql
@@ -0,0 +1,19 @@
+﻿SELECT * FROM tbl_persons 
+WHERE gender = 'M';
+
+SELECT * FROM tbl_persons 
+WHERE age = 18;
+
+SELECT * FROM tbl_persons 
+WHERE address IS Null;
+
+UPDATE tbl_persons
+SET age = age + 1;
+
+DELETE FROM tbl_persons
+WHERE address IS Null;
+
+SELECT COUNT(*) FROM tbl_persons;
+
+SELECT age, COUNT(age) FROM tbl_persons
+GROUP BY age;
