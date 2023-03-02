12873f6bec8d5b98ebc941cd9c4593b4eb11da03
RomanNoname <105872125+RomanNoname@users.noreply.github.com>
2023-03-02 15:28:45 +0200

    Romanbychkov homework34 (#223)
    * Initial Commit
    
    * Added link All Products

---
+++ Roman Bychkov/Lesson34/DataBase/DTO/Order.cs
@@ -0,0 +1,28 @@
+﻿
+namespace DataBase.DTO
+{
+    [Table("orders", Schema = "public")]
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [ForeignKey("person_id")]
+        [Required]
+        public int PersonId { get; set; }
+
+        [ForeignKey("product_id")]
+        [Required]
+        public int ProductId { get; set; }
+
+        [Column("order_time")]
+        [Required]
+        public DateTime OrderTime { get; set; }
+
+        public virtual Person Person { get; set; }
+
+        public virtual Product Product { get; set; }
+    }
+}
