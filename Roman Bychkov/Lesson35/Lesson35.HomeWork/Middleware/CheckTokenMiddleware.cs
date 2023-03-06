2860742ecc5f3bd8e98d32a7ad35cd18c6d70de8
Roman Bychkov <roman.bychkov.1921@gmail.com>
2023-03-06 23:03:10 +0200

    Added Code Status to tokenmiddleware

--- Roman Bychkov/Lesson35/Lesson35.HomeWork/Middleware/CheckTokenMiddleware.cs
+++ Roman Bychkov/Lesson35/Lesson35.HomeWork/Middleware/CheckTokenMiddleware.cs
@@ -3,8 +3,8 @@
     internal class CheckTokenMiddleware
     {
         private readonly RequestDelegate _next;
-        List<string> _whiteList;
-        public CheckTokenMiddleware(RequestDelegate next, List<string> whiteList)
+        List<Guid> _whiteList;
+        public CheckTokenMiddleware(RequestDelegate next, List<Guid> whiteList)
         {
             _next = next;
             _whiteList = whiteList;
@@ -12,12 +12,20 @@
 
         public async Task InvokeAsync(HttpContext context)
         {
-            var token = context.Request.Headers["token"].ToString();
+            var valid = Guid.TryParse(context.Request.Headers["token"].ToString(), out Guid result);
 
-            if (_whiteList.Contains(token))
+            if (valid && _whiteList.Contains(result))
                 await _next.Invoke(context);
+            else if(!valid)
+            {
+                context.Response.StatusCode = 401;
+                context.Response.WriteAsync("Invalid token");
+            }
             else
+            {
+                context.Response.StatusCode = 403;
                 context.Response.WriteAsync("You have not access!");
+            }
         }
     }
 }
--- Roman Bychkov/Lesson35/Lesson35.HomeWork/Program.cs
+++ Roman Bychkov/Lesson35/Lesson35.HomeWork/Program.cs
@@ -26,10 +26,11 @@ app.MapControllers();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseMiddleware<ExceptionMiddleware>();
-app.UseMiddleware<CheckTokenMiddleware>(new List<string>()
-{   Guid.NewGuid().ToString(),
-     Guid.NewGuid().ToString(),
-     Guid.NewGuid().ToString()
+app.UseMiddleware<CheckTokenMiddleware>(new List<Guid>()
+{  
+    Guid.Parse("49c1c358-a0f1-44cd-8fa2-b9a496527bff"),
+    Guid.Parse("69e07543-5d2d-4dd8-8cea-91dae294630b"),
+    Guid.Parse("32d8f66f-2f55-430f-b86a-d3cd713f9fe7")
 });
 
 app.Run();
