0971c62b5972101334e4d483685410dc73b49dcc
shytskyi <111076900+shytskyi@users.noreply.github.com>
2023-03-10 19:10:24 +0100

    HW 34

---
+++ Ivan_Shytskyi/Lesson_34/Lesson_34/DataModel/DTO/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lesson_34.DataModel.DTO
+{
+    [Table("orders", Schema = "public")]
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("PersonId")]
+        [Required]
+        public int PersonId { get; set; }
+
+        [Column("ProductId")]
+        [Required]
+        public int ProductId { get; set; }
+
+        [Column("order_time")]
+        [Required]
+        public DateTime OrderTime { get; set; }
+
+        public virtual Person Person { get; set; }
+
+        public virtual Product Product { get; set; }
+    }
+}
