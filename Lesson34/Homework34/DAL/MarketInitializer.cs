48219e2fd35994715ee5318012778037bcf9d492
unknown <Bonislavskiy@gmail.com>
2023-03-11 21:24:00 -0500

    Homework34 Added

---
+++ Lesson34/Homework34/DAL/MarketInitializer.cs
@@ -0,0 +1,45 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Data.Entity;
+//using Homework34.Models;
+
+//namespace Homework34.DAL
+//{
+//    public class MarketInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<MarketContext>
+//    {
+//        protected override void Seed(MarketContext context)
+//        {
+//            var persons = new List<Person>
+//            {
+//            new Person{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
+//            new Person{FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01")},
+//            new Person{FirstMidName="Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse("2003-09-01")},
+//            new Person{FirstMidName="Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01")},
+//            new Person{FirstMidName="Yan",LastName="Li",EnrollmentDate=DateTime.Parse("2002-09-01")},
+//            new Person{FirstMidName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
+//            new Person{FirstMidName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
+//            new Person{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
+//            };
+
+//            persons.ForEach(s => context.Persons.Add(s));
+//            context.SaveChanges();
+//            var orders = new List<Order>
+//            {
+//            new Order{Title="A lot of ground",Credits=30},
+//            new Order{Title="Plumbering",Credits=10},
+//            new Order{Title = "Timber in stock", Credits = 20 }
+//            };
+//            orders.ForEach(s => context.Orders.Add(s));
+//            context.SaveChanges();
+//            var enrollments = new List<Enrollment>
+//            {
+//            new Enrollment{PersonID=1,OrderID=1,Grade=Grade.Standard},
+//            new Enrollment{PersonID=2,OrderID=2}
+//            };
+//            enrollments.ForEach(s => context.Enrollments.Add(s));
+//            context.SaveChanges();
+//        }
+//    }
+//}
---
+++ Lesson34/Homework34/Models/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Homework34.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        //[PasswordPropertyText]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Phone]
+        public string? Phone { get; set; }
+
+        //public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment>? Enrollments { get; set; }
+    }
+}
+
