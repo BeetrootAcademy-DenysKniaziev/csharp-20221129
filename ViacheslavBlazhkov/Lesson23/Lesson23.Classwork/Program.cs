c1b29ba23de45129a57b119c00784a7e4c6ba006
Viacheslav Blazhkov <79785720+ViacheslavBlazhkov@users.noreply.github.com>
2023-03-16 11:57:11 +0200

    Viacheslav blazhkov lesson 23 (#229)
    * Last commit
    
    * LC
    
    * commit

---
+++ ViacheslavBlazhkov/Lesson23/Lesson23.Classwork/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Lesson23.Classwork
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            var tasks = new List<Task>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var x = i;
+                tasks.Add(Print(x.ToString(), cts.Token));
+            }
+
+            Task.Run(async () =>
+            {
+                await Task.Delay(1800);
+                cts.Cancel();
+            });
+
+            Task.WaitAll(tasks.ToArray(), 10000);
+
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+
+            await GetNumber();
+        }
+
+        static async Task Print(string text, CancellationToken token)
+        {
+            Console.WriteLine("TID: " + Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+            if (token.IsCancellationRequested)
+                return;
+
+            Console.WriteLine(text);
+
+            if (token.IsCancellationRequested)
+                return;
+
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+        }
+
+        static async Task<int> GetNumberAsync()
+        {
+            await Task.Delay(1000);
+            return 10;
+        }
+
+        static ValueTask<int> GetNumber()
+        {
+            return ValueTask.FromResult(10);
+        }
+    }
+}
---
+++ ViacheslavBlazhkov/Lesson23/Lesson23.Homework/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WeatherApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Weather: ");
+            string apiKey = "322c0a7851b8ad7f4f78c1648672ae49";
+
+            Console.WriteLine("Less than 80");
+            Console.Write("Enter Latitude: ");
+            double latitude = double.Parse(Console.ReadLine());
+            Console.Write("Enter Longitude: ");
+            double longitude = double.Parse(Console.ReadLine());
+
+            using (var client = new HttpClient())
+            {
+                string url = $"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={apiKey}&units=metric";
+                var response = await client.GetAsync(url);
+
+                string json = await response.Content.ReadAsStringAsync();
+                dynamic data = JsonConvert.DeserializeObject(json);
+
+                string location = data.name;
+                string description = data.weather[0].description;
+                double temperature = data.main.temp;
+
+                Console.WriteLine($"Current weather in {location}: {description}, {temperature}°C");
+            }
+
+            Console.Write("\nCats fact: ");
+            using(var client = new HttpClient())
+            {
+                string url = "https://meowfacts.herokuapp.com/?lang=ukr";
+                var response = await client.GetAsync(url);
+
+                string json = await response.Content.ReadAsStringAsync();
+                dynamic data = JsonConvert.DeserializeObject(json);
+                dynamic fact = data;
+
+                Console.WriteLine(fact.data[0]);
+            }
+        }
+    }
+}
