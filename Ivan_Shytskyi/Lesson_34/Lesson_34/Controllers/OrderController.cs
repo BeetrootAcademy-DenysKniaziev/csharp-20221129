7af4a96787dae79dddb010c87dc4451560afbb2a
shytskyi <111076900+shytskyi@users.noreply.github.com>
2023-03-28 13:43:30 +0200

    HW 34 (#237)

---
+++ Ivan_Shytskyi/Lesson_34/Lesson_34/Controllers/OrderController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Lesson_34.DataModel;
+using Lesson_34.DataModel.DTO;
+
+namespace Lesson_34.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Order
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Orders.Include(o => o.Person).Include(o => o.Product);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Order/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .Include(o => o.Person)
+                .Include(o => o.Product)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // GET: Order/Create
+        public IActionResult Create()
+        {
+            ViewData["PersonId"] = new SelectList(_context.Persons, "Id", "FirstName");
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Description");
+            return View();
+        }
+
+        // POST: Order/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,PersonId,ProductId,OrderTime")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(order);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PersonId"] = new SelectList(_context.Persons, "Id", "FirstName", order.PersonId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Description", order.ProductId);
+            return View(order);
+        }
+
+        // GET: Order/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            ViewData["PersonId"] = new SelectList(_context.Persons, "Id", "FirstName", order.PersonId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Description", order.ProductId);
+            return View(order);
+        }
+
+        // POST: Order/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,PersonId,ProductId,OrderTime")] Order order)
+        {
+            if (id != order.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PersonId"] = new SelectList(_context.Persons, "Id", "FirstName", order.PersonId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Description", order.ProductId);
+            return View(order);
+        }
+
+        // GET: Order/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .Include(o => o.Person)
+                .Include(o => o.Product)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // POST: Order/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Orders == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Orders'  is null.");
+            }
+            var order = await _context.Orders.FindAsync(id);
+            if (order != null)
+            {
+                _context.Orders.Remove(order);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderExists(int id)
+        {
+          return _context.Orders.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Ivan_Shytskyi/Lesson_34/Lesson_34/DataModel/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Lesson_34.DataModel.DTO;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lesson_34.DataModel
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseNpgsql("Server=host.docker.internal;Port=32768;Database=SHOP;User Id=postgres;Password=postgrespw;")
+                .LogTo(text => File.AppendAllText("log.txt", text));
+        }
+    }
+}
