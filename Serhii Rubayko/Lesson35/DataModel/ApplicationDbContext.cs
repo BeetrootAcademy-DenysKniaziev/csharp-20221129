abea9e5f22c078cd3da11cef646dab138fb104a2
SkyLineR1001 <119454635+SkyLineR1001@users.noreply.github.com>
2023-04-10 15:12:38 +0300

    Serhii rubayko less34 (#247)
    * Lesson 34
    
    * AspNet
    
    * Adding Products controller
    
    * ShopDomain
    
    * Delete unnecessary blocks

---
+++ Serhii Rubayko/Lesson35/DataModel/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Lesson35.DataModel.DTO;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Lesson35.DataModel
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            //base.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseNpgsql("Server=host.docker.internal;Port=32768;Database=shop_new;User Id=postgres;" +
+                            "Password=postgrespw;");
+                //.LogTo(text => File.AppendAllText("log.txt", text));
+        }
+    }
+}
+
